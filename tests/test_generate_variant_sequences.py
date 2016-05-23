25d9a6288498f0afe63396a93b5e13923d19efd4
Susanna Kiwala <ssiebert@genome.wustl.edu>
2016-05-23 13:59:50 -0500

    Better structure for tests

--- tests/test_generate_variant_sequences.py
+++ tests/test_generate_variant_sequences.py
@@ -5,22 +5,28 @@ from subprocess import call
 from filecmp import cmp
 
 class GenerateVariantSequences(unittest.TestCase):
-    def runTest(self):
+    def setUp(self):
         base_dir = os.path.abspath(os.path.join(os.path.dirname(os.path.realpath(__file__)), '..'))
-        executable_dir = os.path.join(base_dir, 'pvac_seq')
-        test_data_dir  = os.path.join(base_dir, 'test_data')
+        self.executable_dir = os.path.join(base_dir, 'pvac_seq')
+        self.test_data_dir  = os.path.join(base_dir, 'test_data')
+        self.sample_name             = 'Test'
+        self.peptide_sequence_length = 21
 
-        sample_name             = 'Test'
-        peptide_sequence_length = 21
+    def tearDown(self):
+        del self.executable_dir
+        del self.test_data_dir
+        del self.sample_name
+        del self.peptide_sequence_length
 
-        generate_variant_sequences_input_file  = os.path.join(test_data_dir, 'annotated_variants.tsv')
+    def test_generate_variant_sequences(self):
+        generate_variant_sequences_input_file  = os.path.join(self.test_data_dir, 'annotated_variants.tsv')
         generate_variant_sequences_output_file = tempfile.NamedTemporaryFile().name
-        generate_variant_sequences_executable  = os.path.join(executable_dir, 'generate_variant_sequences.py')
+        generate_variant_sequences_executable  = os.path.join(self.executable_dir, 'generate_variant_sequences.py')
 
-        generate_variant_sequences_command = "python %s %s %s %s" % (generate_variant_sequences_executable, generate_variant_sequences_input_file, peptide_sequence_length, generate_variant_sequences_output_file)
+        generate_variant_sequences_command = "python %s %s %s %s" % (generate_variant_sequences_executable, generate_variant_sequences_input_file, self.peptide_sequence_length, generate_variant_sequences_output_file)
 
         self.assertFalse(call(generate_variant_sequences_command, shell=True), 'GenerateVariantSequences command executes successfully')
-        expected_output_file = os.path.join(test_data_dir, ("%s_%s.fa" % (sample_name, peptide_sequence_length)))
+        expected_output_file = os.path.join(self.test_data_dir, ("%s_%s.fa" % (self.sample_name, self.peptide_sequence_length)))
         self.assertTrue(cmp(generate_variant_sequences_output_file, expected_output_file), 'GenerateVariantSequences output as expected')
 
 if __name__ == '__main__':
