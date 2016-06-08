a425068d53cc49146962e783806b1f9d29f40410
agraubert <captianjroot@live.com>
2016-06-08 14:40:01 -0500

    made revisions to setup.py configuration

--- setup.py
+++ setup.py
@@ -8,5 +8,12 @@ setup(
         "console_scripts":[
             "pVAC-Seq = src.pVAC_Seq:main"
         ]
-    }
+    },
+
+    author = "Griffith Lab",
+    author_email = "abc@email.com",
+    description = "A cancer immunotherapy pipeline for the identification of personalized Variant Antigens by Cancer Sequencing (pVAC-Seq)",
+    license = "NPOSL-3.0",
+    keywords = "antigens neoantigens cancer sequencing variant variants",
+    url = "https://github.com/griffithlab/pVAC-Seq",   #
 )
