2d5673eba72f94eb3808817e52d224f745105426
agraubert <captianjroot@live.com>
2016-06-08 16:09:23 -0500

    Added titles to argument parsers to clarify which script is printing a help message

--- src/pvacseq/main.py
+++ src/pvacseq/main.py
@@ -9,7 +9,7 @@ except ValueError:
 
 
 def main(args_input = sys.argv[1:]):
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser("pVAC-Seq Main")
 
     parser.add_argument("input",
                         help="Input TSV File with variants (please provide complete path)"
