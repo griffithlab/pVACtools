57888cbf74e04b4ec176a3f123dd62b162b71a3c
agraubert <captianjroot@live.com>
2016-06-09 10:09:22 -0500

    Added classifiers to setup.py for better classification on pypi
    Removed unrecognized license classifier
    
    Modified setup configuration to match new changes to package name
    
    changed setup.py configuration again

--- setup.py
+++ setup.py
@@ -1,18 +1,27 @@
 from setuptools import setup
 
 setup(
-    name="pVAC-Seq",
-    version="1.5",
-    packages=["pvac_seq", "pvac_seq.lib"],
+    name="pvacseq",
+    version="1.0.1",
+    packages=["pvacseq", "pvacseq.lib"],
     entry_points={
         "console_scripts":[
-            "pVAC-Seq = pvac_seq.pVAC_Seq:main"
+            "pvacseq = pvacseq.pvacseq:main"
         ]
     },
+    classifiers=[
+        'Development Status :: 4 - Beta',
+
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering :: Bio-Informatics',
+
+        "Programming Language :: Python :: 3.5"
+    ],
 
     author = "Griffith Lab",
-    author_email = "abc@email.com",
-    description = "A cancer immunotherapy pipeline for the identification of personalized Variant Antigens by Cancer Sequencing (pVAC-Seq)",
+    author_email = "pvacseq-support@genome.wustl.edu",
+    description = "Personalized Variant Antigens by Cancer Sequencing (pVAC-Seq)",
+    long_description = "A cancer immunotherapy pipeline for the identification of personalized Variant Antigens by Cancer Sequencing (pVAC-Seq)",
     license = "NPOSL-3.0",
     keywords = "antigens neoantigens cancer sequencing variant variants",
     url = "https://github.com/griffithlab/pVAC-Seq",   #
