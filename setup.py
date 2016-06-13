0fc1ff95e9c8d53d74058ee7db03d5f598796655
agraubert <captianjroot@live.com>
2016-06-13 09:48:03 -0500

    Updated author list, and added python version checking

--- setup.py
+++ setup.py
@@ -1,8 +1,15 @@
 from setuptools import setup
 
+import sys
+if sys.version_info < (3,5):
+    print("This python version is not supported:")
+    print(sys.version)
+    print("pVAC-Seq requires python 3.5 or greater")
+    sys.exit(1)
+
 setup(
     name="pvacseq",
-    version="1.0.1",
+    version="1.1.6",
     packages=["pvacseq", "pvacseq.lib"],
     entry_points={
         "console_scripts":[
@@ -18,7 +25,7 @@ setup(
         "Programming Language :: Python :: 3.5"
     ],
 
-    author = "Griffith Lab",
+    author = "Jasreet Hundal, Susanna Kiwala, Aaron Graubert, Jason Walker, Chris Miller, Malachi Griffith and Elaine Mardis",
     author_email = "pvacseq-support@genome.wustl.edu",
     description = "Personalized Variant Antigens by Cancer Sequencing (pVAC-Seq)",
     long_description = "A cancer immunotherapy pipeline for the identification of personalized Variant Antigens by Cancer Sequencing (pVAC-Seq)",
