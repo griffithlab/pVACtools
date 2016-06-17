fe0c5498d3b36c292536e3a1fbaabe9bc5d75e84
Susanna Kiwala <ssiebert@genome.wustl.edu>
2016-06-17 11:22:51 -0500

    example_data folder needs to be under the main pvacseq folder in order to be installed during packaging

--- pvacseq/lib/download_example_data.py
+++ pvacseq/lib/download_example_data.py
@@ -8,7 +8,7 @@ def main(args_input = sys.argv[1:]):
     parser.add_argument('destination_directory', help='Directory for downloading example data',)
     args = parser.parse_args(args_input)
 
-    base_dir = os.path.abspath(os.path.join(os.path.dirname(os.path.realpath(__file__)), '..', '..'))
+    base_dir = os.path.abspath(os.path.join(os.path.dirname(os.path.realpath(__file__)), '..'))
     source_directory = os.path.join(base_dir, 'example_data')
     copytree(source_directory, os.path.join(args.destination_directory, 'example_data'))
 
