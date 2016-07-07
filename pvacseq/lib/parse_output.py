4d16fed9d57cc4522785ef44684632414cb715fa
Susanna Kiwala <ssiebert@genome.wustl.edu>
2016-07-07 09:28:40 -0500

    Rename netmhc variables to iedb

--- pvacseq/lib/parse_output.py
+++ pvacseq/lib/parse_output.py
@@ -46,14 +46,14 @@ def parse_input_tsv_file(input_tsv_file):
         tsv_entries[line['index']] = line
     return tsv_entries
 
-def match_wildtype_and_mutant_entries(netmhc_results, wt_netmhc_results):
-    for key, result in netmhc_results.items():
-        (wt_netmhc_result_key, mt_position) = key.split('|', 1)
+def match_wildtype_and_mutant_entries(iedb_results, wt_iedb_results):
+    for key, result in iedb_results.items():
+        (wt_iedb_result_key, mt_position) = key.split('|', 1)
         if result['variant_type'] == 'missense':
-            netmhc_results[key]['wt_epitope_seq'] = wt_netmhc_results[wt_netmhc_result_key][mt_position]['wt_epitope_seq']
-            netmhc_results[key]['wt_score']       = wt_netmhc_results[wt_netmhc_result_key][mt_position]['wt_score']
+            iedb_results[key]['wt_epitope_seq'] = wt_iedb_results[wt_iedb_result_key][mt_position]['wt_epitope_seq']
+            iedb_results[key]['wt_score']       = wt_iedb_results[wt_iedb_result_key][mt_position]['wt_score']
         else:
-            wt_results        = wt_netmhc_results[wt_netmhc_result_key]
+            wt_results        = wt_iedb_results[wt_iedb_result_key]
             mt_epitope_seq    = result['mt_epitope_seq']
             best_match_count  = 0
             best_left_padding = 0
@@ -69,20 +69,20 @@ def match_wildtype_and_mutant_entries(netmhc_results, wt_netmhc_results):
                     best_left_padding   = left_padding
                     best_match_position = wt_position
 
-            netmhc_results[key]['wt_epitope_seq'] = 'NA'
-            netmhc_results[key]['wt_score']       = 'NA'
+            iedb_results[key]['wt_epitope_seq'] = 'NA'
+            iedb_results[key]['wt_score']       = 'NA'
             if best_match_count >= min_match_count(len(wt_result['wt_epitope_seq'])):
-                netmhc_results[key]['wt_epitope_seq'] = wt_netmhc_results[wt_netmhc_result_key][best_match_position]['wt_epitope_seq']
-                netmhc_results[key]['wt_score']       = wt_netmhc_results[wt_netmhc_result_key][best_match_position]['wt_score']
+                iedb_results[key]['wt_epitope_seq'] = wt_iedb_results[wt_iedb_result_key][best_match_position]['wt_epitope_seq']
+                iedb_results[key]['wt_score']       = wt_iedb_results[wt_iedb_result_key][best_match_position]['wt_score']
 
-    return netmhc_results
+    return iedb_results
 
-def parse_netmhc_file(input_netmhc_file, tsv_entries, key_file):
-    tsv_reader = csv.reader(input_netmhc_file, delimiter='\t')
+def parse_iedb_file(input_iedb_file, tsv_entries, key_file):
+    tsv_reader = csv.reader(input_iedb_file, delimiter='\t')
     protein_identifier_from_label = protein_identifier_for_label(key_file)
     pattern = re.compile('NetMHC|Protein')
-    netmhc_results = {}
-    wt_netmhc_results = {}
+    iedb_results = {}
+    wt_iedb_results = {}
     for line in tsv_reader:
         if len(line) == 0 or pattern.match(line[0]):
             continue
@@ -98,28 +98,28 @@ def parse_netmhc_file(input_netmhc_file, tsv_entries, key_file):
         if protein_type == 'MT':
             tsv_entry = tsv_entries[tsv_index]
             key = "%s|%s" % (tsv_index, position)
-            if key not in netmhc_results:
-                netmhc_results[key] = {}
-            netmhc_results[key]['mt_score']          = int(score)
-            netmhc_results[key]['mt_epitope_seq']    = epitope
-            netmhc_results[key]['gene_name']         = tsv_entry['gene_name']
-            netmhc_results[key]['amino_acid_change'] = tsv_entry['amino_acid_change']
-            netmhc_results[key]['variant_type']      = tsv_entry['variant_type']
-            netmhc_results[key]['position']          = position
-            netmhc_results[key]['tsv_index']         = tsv_index
+            if key not in iedb_results:
+                iedb_results[key] = {}
+            iedb_results[key]['mt_score']          = int(score)
+            iedb_results[key]['mt_epitope_seq']    = epitope
+            iedb_results[key]['gene_name']         = tsv_entry['gene_name']
+            iedb_results[key]['amino_acid_change'] = tsv_entry['amino_acid_change']
+            iedb_results[key]['variant_type']      = tsv_entry['variant_type']
+            iedb_results[key]['position']          = position
+            iedb_results[key]['tsv_index']         = tsv_index
 
         if protein_type == 'WT':
-            if tsv_index not in wt_netmhc_results:
-                wt_netmhc_results[tsv_index] = {}
-            wt_netmhc_results[tsv_index][position] = {}
-            wt_netmhc_results[tsv_index][position]['wt_score']       = int(score)
-            wt_netmhc_results[tsv_index][position]['wt_epitope_seq'] = epitope
+            if tsv_index not in wt_iedb_results:
+                wt_iedb_results[tsv_index] = {}
+            wt_iedb_results[tsv_index][position] = {}
+            wt_iedb_results[tsv_index][position]['wt_score']       = int(score)
+            wt_iedb_results[tsv_index][position]['wt_epitope_seq'] = epitope
 
-    return match_wildtype_and_mutant_entries(netmhc_results, wt_netmhc_results)
+    return match_wildtype_and_mutant_entries(iedb_results, wt_iedb_results)
 
-def flatten_netmhc_results(netmhc_results):
-    #transform the netmhc_results dictionary into a two-dimensional list
-    flattened_netmhc_results = list((
+def flatten_iedb_results(iedb_results):
+    #transform the iedb_results dictionary into a two-dimensional list
+    flattened_iedb_results = list((
         value['gene_name'],
         value['amino_acid_change'],
         value['position'],
@@ -128,31 +128,31 @@ def flatten_netmhc_results(netmhc_results):
         value['wt_epitope_seq'],
         value['mt_epitope_seq'],
         value['tsv_index'],
-    ) for value in netmhc_results.values())
+    ) for value in iedb_results.values())
 
-    return flattened_netmhc_results
+    return flattened_iedb_results
 
-def sort_netmhc_results(flattened_netmhc_results):
-    sorted_netmhc_results = sorted(
-        flattened_netmhc_results,
-        key=lambda flattened_netmhc_results: (flattened_netmhc_results[0], flattened_netmhc_results[1], flattened_netmhc_results[3], " ".join(str(item) for item in flattened_netmhc_results))
+def sort_iedb_results(flattened_iedb_results):
+    sorted_iedb_results = sorted(
+        flattened_iedb_results,
+        key=lambda flattened_iedb_results: (flattened_iedb_results[0], flattened_iedb_results[1], flattened_iedb_results[3], " ".join(str(item) for item in flattened_iedb_results))
     )
 
-    return sorted_netmhc_results
+    return sorted_iedb_results
 
-def process_input_netmhc_file(input_netmhc_file, tsv_entries, key_file):
-    netmhc_results           = parse_netmhc_file(input_netmhc_file, tsv_entries, key_file)
-    flattened_netmhc_results = flatten_netmhc_results(netmhc_results)
-    sorted_netmhc_results    = sort_netmhc_results(flattened_netmhc_results)
+def process_input_iedb_file(input_iedb_file, tsv_entries, key_file):
+    iedb_results           = parse_iedb_file(input_iedb_file, tsv_entries, key_file)
+    flattened_iedb_results = flatten_iedb_results(iedb_results)
+    sorted_iedb_results    = sort_iedb_results(flattened_iedb_results)
 
-    return sorted_netmhc_results
+    return sorted_iedb_results
 
 def output_headers():
     return['Chromosome', 'Start', 'Stop', 'Reference', 'Variant', 'Transcript', 'Ensembl Gene ID', 'Variant Type', 'Mutation', 'Protein Position', 'Gene Name', 'HLA Allele', 'Peptide Length', 'Sub-peptide Position', 'MT score', 'WT score', 'MT epitope seq', 'WT epitope seq', 'Fold Change']
 
 def main(args_input = sys.argv[1:]):
     parser = argparse.ArgumentParser('pvacseq parse_output')
-    parser.add_argument('input_netmhc_file', type=argparse.FileType('r'), help='Raw output file from Netmhc',)
+    parser.add_argument('input_iedb_file', type=argparse.FileType('r'), help='Raw output file from Netmhc',)
     parser.add_argument('input_tsv_file', type=argparse.FileType('r'), help='Input list of variants')
     parser.add_argument('key_file', type=argparse.FileType('r'), help='Key file for lookup of FASTA IDs')
     parser.add_argument('output_file', type=argparse.FileType('w'), help='Parsed output file')
@@ -161,12 +161,12 @@ def main(args_input = sys.argv[1:]):
     tsv_writer = csv.DictWriter(args.output_file, delimiter='\t', fieldnames=output_headers())
     tsv_writer.writeheader()
 
-    basename = os.path.basename(args.input_netmhc_file.name)
+    basename = os.path.basename(args.input_iedb_file.name)
     (sample, allele, peptide_length, rest) = basename.split(".", 3)
 
-    tsv_entries    = parse_input_tsv_file(args.input_tsv_file)
-    netmhc_results = process_input_netmhc_file(args.input_netmhc_file, tsv_entries, args.key_file)
-    for gene_name, variant_aa, position, mt_score, wt_score, wt_epitope_seq, mt_epitope_seq, tsv_index in netmhc_results:
+    tsv_entries  = parse_input_tsv_file(args.input_tsv_file)
+    iedb_results = process_input_iedb_file(args.input_iedb_file, tsv_entries, args.key_file)
+    for gene_name, variant_aa, position, mt_score, wt_score, wt_epitope_seq, mt_epitope_seq, tsv_index in iedb_results:
         tsv_entry = tsv_entries[tsv_index]
         if mt_epitope_seq != wt_epitope_seq:
             if wt_epitope_seq == 'NA':
