288c6d86dcf214a7a877c6e7c0b57174768c5acf
agraubert <captianjroot@live.com>
2016-07-11 12:12:11 -0500

    Bumped version to 2.0.1

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ if sys.version_info < (3,5):
 
 setup(
     name="pvacseq",
-    version="2.0.0",
+    version="2.0.1",
     packages=["pvacseq", "pvacseq.lib"],
     entry_points={
         "console_scripts":[
