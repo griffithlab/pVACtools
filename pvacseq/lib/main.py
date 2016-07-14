4953736a985795bedf7f291ef228d2b53084efb3
Susanna Kiwala <ssiebert@genome.wustl.edu>
2016-07-14 08:54:27 -0500

    Cleanup temporary directory after use

--- pvacseq/lib/main.py
+++ pvacseq/lib/main.py
@@ -143,6 +143,7 @@ def main(args_input = sys.argv[1:]):
                                         tsv_writer.writerow(row)
                         iedb_output_files.append(iedb_out)
                         print("Completed")
+    tmp_dir.cleanup()
 
     input_files = []
     tmp_dir.cleanup()
