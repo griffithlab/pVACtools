e9b647017270f4ec0d55ebf529701136adc078b6
Susanna Kiwala <ssiebert@genome.wustl.edu>
2016-08-12 10:42:13 -0500

    Update formatting

--- pvacseq/lib/main.py
+++ pvacseq/lib/main.py
@@ -17,76 +17,109 @@ import shutil
 def main(args_input = sys.argv[1:]):
     parser = argparse.ArgumentParser("pvacseq run")
 
-    parser.add_argument("input_file",
-                        help="Input VCF with VEP annotations (please provide complete path)"
-                        )
-    parser.add_argument("sample_name",
-                        help="Name of Sample; will be used as prefix for output files"
-                        )
-    parser.add_argument("allele",
-                        help="Allele name to predict epitope prediction. Multiple alleles can be specified using a comma-separated list. For a list of available alleles, use: pvacseq valid_alleles",
-                        type=lambda s:[a for a in s.split(',')]
-                        )
-    parser.add_argument("epitope_length",
-                        help="length of subpeptides(epitopes) to predict ; Multiple lengths can be specified using a comma-separated list. Typical epitope lengths vary between 8-11.",
-                        type=lambda s:[int(epl) for epl in s.split(',')]
-                        )
-    parser.add_argument("prediction_algorithms",
-                        choices=PredictionClass.prediction_methods(),
-                        nargs="+",
-                        help="The epitope prediction algorithms to use",
-                        )
-    parser.add_argument("output_dir",
-                        help="Output directory for writing all result files"
-                        )
-    parser.add_argument("-l", "--peptide-sequence-length",
-                        type=int,
-                        help="length of the peptide sequences in the input FASTA file; default 21",
-                        default=21)
-    parser.add_argument('-g', '--gene-expn-file', help='genes.fpkm_tracking file from Cufflinks')
-    parser.add_argument('-i', '--transcript-expn-file', help='isoforms.fpkm_tracking file from Cufflinks')
-    parser.add_argument('--net-chop-method',
-                        choices=lib.net_chop.methods,
-                        help="NetChop prediction method to use (\"cterm\" for C term 3.0, \"20s\" for 20S 3.0).  Default: \"cterm\" (C term 3.0)",
-                        default=None)
+    parser.add_argument(
+        "input_file",
+        help="Input VCF with VEP annotations (please provide complete path)"
+    )
+    parser.add_argument(
+        "sample_name",
+        help="Name of Sample; will be used as prefix for output files"
+    )
+    parser.add_argument(
+        "allele", type=lambda s:[a for a in s.split(',')],
+        help="Allele name to predict epitope prediction. "
+             + "Multiple alleles can be specified using a comma-separated list. "
+             + "For a list of available alleles, use: pvacseq valid_alleles",
+    )
+    parser.add_argument(
+        "epitope_length", type=lambda s:[int(epl) for epl in s.split(',')],
+        help="Length of subpeptides(epitopes) to predict. "
+             + "Multiple lengths can be specified using a comma-separated list. "
+             + "Typical epitope lengths vary between 8-11.",
+    )
+    parser.add_argument(
+        "prediction_algorithms",
+        choices=PredictionClass.prediction_methods(),
+        nargs="+",
+        help="The epitope prediction algorithms to use",
+    )
+    parser.add_argument(
+        "output_dir",
+        help="Output directory for writing all result files"
+    )
+    parser.add_argument(
+        "-l", "--peptide-sequence-length", type=int,
+        default=21,
+        help="length of the peptide sequences in the input FASTA file. Default: 21",
+    )
+    parser.add_argument(
+        '-g', '--gene-expn-file',
+        help='genes.fpkm_tracking file from Cufflinks'
+    )
+    parser.add_argument(
+        '-i', '--transcript-expn-file',
+        help='isoforms.fpkm_tracking file from Cufflinks'
+    )
+    parser.add_argument(
+        '--net-chop-method',
+        choices=lib.net_chop.methods,
+        default=None,
+        help="NetChop prediction method to use (\"cterm\" for C term 3.0, \"20s\" for 20S 3.0). "
+             + "Default: \"cterm\" (C term 3.0)",
+    )
     parser.add_argument(
         '--netmhc-stab',
         action='store_true',
         help="Run NetMHCStabPan after all filtering and add stability predictions to predicted epitopes"
     )
-    parser.add_argument('-t', '--top-result-per-mutation',
-                        action='store_true',
-                        help='Output top scoring candidate per allele-length per mutation. Default: False')
-    parser.add_argument('-m', '--top-score-metric',
-                        choices=['lowest', 'median'],
-                        default='median',
-                        help="The ic50 scoring metric to use when filtering epitopes. " +
-                        "lowest: Best MT Score - lowest MT ic50 binding score of all chosen prediction methods. " +
-                        "median: Median MT Score All Methods - median MT ic50 binding score of all chosen prediction methods. " +
-                        "Default: median")
-    parser.add_argument("-b","--binding-threshold",
-                        type=int,
-                        help="report only epitopes where the mutant allele has ic50 binding scores below this value ; default 500",
-                        default=500)
-    parser.add_argument("-c", "--minimum-fold-change",
-                        type=int,
-                        help="Minimum fold change between mutant binding score and wild-type score. The default is 0, which filters no results, but 1 is often a sensible default (requiring that binding is better to the MT than WT)",
-                        default=0)
-    parser.add_argument('--expn-val', type=int,
-                        help="Gene Expression (FPKM) Cutoff. " +
-                        "default 1",
-                        default=1)
-    parser.add_argument('--net-chop-threshold',
-                        type=float,
-                        help="NetChop prediction threshold.  Default: 0.5",
-                        default=0.5)
-    parser.add_argument("-s", "--fasta-size",
-                        type=int,
-                        help="Number of fasta entries per IEDB request. For some resource-intensive prediction algorithms like Pickpocket and NetMHC it might be helpful to reduce this number. Needs to be an even number.",
-                        default=200)
-    parser.add_argument("-k", "--keep-tmp-files",
-                        action='store_true',
-                        help="Keep intermediate output files.",)
+    parser.add_argument(
+        '-t', '--top-result-per-mutation',
+        action='store_true',
+        help='Output top scoring candidate per allele-length per mutation. Default: False'
+    )
+    parser.add_argument(
+        '-m', '--top-score-metric',
+        choices=['lowest', 'median'],
+        default='median',
+        help="The ic50 scoring metric to use when filtering epitopes. "
+             + "lowest: Best MT Score - lowest MT ic50 binding score of all chosen prediction methods. "
+             + "median: Median MT Score All Methods - median MT ic50 binding score of all chosen prediction methods. "
+             + "Default: median"
+        )
+    parser.add_argument(
+        "-b","--binding-threshold", type=int,
+        default=500,
+        help="Report only epitopes where the mutant allele has ic50 binding scores below this value. Default: 500",
+    )
+    parser.add_argument(
+        "-c", "--minimum-fold-change", type=int,
+        default=0,
+        help="Minimum fold change between mutant binding score and wild-type score. "
+             + "The default is 0, which filters no results, but 1 is often a sensible choice "
+             + "(requiring that binding is better to the MT than WT)",
+    )
+    parser.add_argument(
+        '--expn-val', type=int,
+        default=1,
+        help="Gene Expression (FPKM) Cutoff. Default: 1",
+    )
+    parser.add_argument(
+        '--net-chop-threshold', type=float,
+        default=0.5,
+        help="NetChop prediction threshold. Default: 0.5",
+    )
+    parser.add_argument(
+        "-s", "--fasta-size",type=int,
+        default=200,
+        help="Number of fasta entries per IEDB request. "
+             + "For some resource-intensive prediction algorithms like Pickpocket and NetMHC it might be helpful to reduce this number. "
+             + "Needs to be an even number.",
+    )
+    parser.add_argument(
+        "-k", "--keep-tmp-files",
+        action='store_true',
+        help="Keep intermediate output files.",
+    )
 
     args = parser.parse_args(args_input)
 
--- pvacseq/pvacseq.py
+++ pvacseq/pvacseq.py
@@ -14,79 +14,109 @@ def main():
     subparsers = parser.add_subparsers()
 
     #add subcommands
-    run_main_program_parser = subparsers.add_parser("run",
-                                                    help="Runs the pVAC-Seq pipeline",
-                                                    add_help=False)
+    run_main_program_parser = subparsers.add_parser(
+        "run",
+        help="Runs the pVAC-Seq pipeline",
+        add_help=False
+    )
     run_main_program_parser.set_defaults(func=lib.main)
 
-    convert_vcf_parser = subparsers.add_parser("convert_vcf",
-                                               help="Converts a VCF into TSV format for downstream steps",
-                                               add_help=False)
+    convert_vcf_parser = subparsers.add_parser(
+        "convert_vcf",
+        help="Converts a VCF into TSV format for downstream steps",
+        add_help=False
+    )
     convert_vcf_parser.set_defaults(func=lib.convert_vcf)
 
-    variant_sequences_parser = subparsers.add_parser("generate_fasta",
-                                                     help="Generates a variant peptide FASTA file from the TSV input file",
-                                                     add_help=False)
+    variant_sequences_parser = subparsers.add_parser(
+        "generate_fasta",
+        help="Generates a variant peptide FASTA file from the TSV input file",
+        add_help=False
+    )
     variant_sequences_parser.set_defaults(func=lib.generate_fasta)
 
-    fasta_key_parser = subparsers.add_parser("generate_fasta_key",
-                                             help="Generates a FASTA key file",
-                                             add_help=False)
+    fasta_key_parser = subparsers.add_parser(
+        "generate_fasta_key",
+        help="Generates a FASTA key file",
+        add_help=False
+    )
     fasta_key_parser.set_defaults(func=lib.generate_fasta_key)
 
-    call_iedb_parser = subparsers.add_parser("call_iedb",
-                                             help="Make epitope binding predicitions using IEDB",
-                                             add_help=False)
+    call_iedb_parser = subparsers.add_parser(
+        "call_iedb",
+        help="Make epitope binding predicitions using IEDB",
+        add_help=False
+    )
     call_iedb_parser.set_defaults(func=lib.call_iedb)
 
-    parse_output_parser = subparsers.add_parser("parse_output",
-                                                help="Parses output from IEDB",
-                                                add_help=False)
+    parse_output_parser = subparsers.add_parser(
+        "parse_output",
+        help="Parses output from IEDB",
+        add_help=False
+    )
     parse_output_parser.set_defaults(func=lib.parse_output)
 
-    combine_parsed_outputs_parser = subparsers.add_parser("combine_parsed_outputs",
-                                                          help="Combines the parsed output files into one file",
-                                                          add_help=False)
+    combine_parsed_outputs_parser = subparsers.add_parser(
+        "combine_parsed_outputs",
+        help="Combines the parsed output files into one file",
+        add_help=False
+    )
     combine_parsed_outputs_parser.set_defaults(func=lib.combine_parsed_outputs)
 
-    binding_filter_parser = subparsers.add_parser("binding_filter",
-                                                  help="Filters variants processed by IEDB by binding score",
-                                                  add_help=False)
+    binding_filter_parser = subparsers.add_parser(
+        "binding_filter",
+        help="Filters variants processed by IEDB by binding score",
+        add_help=False
+    )
     binding_filter_parser.set_defaults(func=lib.binding_filter)
 
-    coverage_filter_parser = subparsers.add_parser("coverage_filter",
-                                                   help="Filters variants processed by IEDB by coverage, vaf, and gene expression",
-                                                   add_help=False)
+    coverage_filter_parser = subparsers.add_parser(
+        "coverage_filter",
+        help="Filters variants processed by IEDB by coverage, vaf, and gene expression",
+        add_help=False
+    )
     coverage_filter_parser.set_defaults(func=lib.coverage_filter)
 
-    net_chop_parser = subparsers.add_parser("net_chop",
-                                            help="Submits epitope predictions to NetChop and records the best cleavage site for each variant",
-                                            add_help=False)
+    net_chop_parser = subparsers.add_parser(
+        "net_chop",
+        help="Submits epitope predictions to NetChop and records the best cleavage site for each variant",
+        add_help=False
+    )
     net_chop_parser.set_defaults(func=lib.net_chop)
 
-    netmhc_stab_parser = subparsers.add_parser("netmhc_stab",
-                                               help="Generates stability predictions for predicted epitopes",
-                                               add_help=False)
+    netmhc_stab_parser = subparsers.add_parser(
+        "netmhc_stab",
+        help="Generates stability predictions for predicted epitopes",
+        add_help=False
+    )
     netmhc_stab_parser.set_defaults(func=lib.netmhc_stab)
 
-    download_example_data_parser = subparsers.add_parser("download_example_data",
-                                                         help="Downloads example input and output files",
-                                                         add_help=False)
+    download_example_data_parser = subparsers.add_parser(
+        "download_example_data",
+        help="Downloads example input and output files",
+        add_help=False
+    )
     download_example_data_parser.set_defaults(func=lib.download_example_data)
 
-    install_vep_plugin_parser = subparsers.add_parser("install_vep_plugin",
-                                                      help="Installs the Wildtype VEP plugin into your VEP_plugins directory",
-                                                      add_help=False)
+    install_vep_plugin_parser = subparsers.add_parser(
+        "install_vep_plugin",
+        help="Installs the Wildtype VEP plugin into your VEP_plugins directory",
+        add_help=False
+    )
     install_vep_plugin_parser.set_defaults(func=lib.install_vep_plugin)
 
-    valid_alleles_parser = subparsers.add_parser("valid_alleles",
-                                                 help="Shows a list of valid allele names",
-                                                 add_help=False)
+    valid_alleles_parser = subparsers.add_parser(
+        "valid_alleles",
+        help="Shows a list of valid allele names",
+        add_help=False
+    )
     valid_alleles_parser.set_defaults(func=lib.valid_alleles)
 
-    parser.add_argument("-v", "--version",
-                        help="Display the currently installed pvacseq version",
-                        action="store_true")
+    parser.add_argument(
+        "-v", "--version",
+        action="store_true",
+        help="Display the currently installed pvacseq version",
+    )
 
     args = parser.parse_known_args()
     if args[0].version is True:
