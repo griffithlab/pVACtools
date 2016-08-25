4920a702dd934c21ab13eae8463315e220f35ba2
agraubert <captianjroot@live.com>
2016-08-25 14:38:07 -0500

    Server processes now live beyond the server
    On launch, the server will load a list of old processes, so it can continue to read from their logs
    
    Fixed server package data
    
    I though I had already fixed it, which is kinda odd

--- pvacseq/server/controllers/default_controller.py
+++ pvacseq/server/controllers/default_controller.py
@@ -2,18 +2,47 @@ import os
 import csv
 import sys
 import itertools
-import multiprocessing as mp
 import subprocess
 import re
 import tempfile
+import json
 
 spinner = re.compile(r'[\\\b\-/|]{2,}')
 allele_file = None
 
-children = {}
-staging_files = []
-#data['processid']=-1
+children = {} #Holds popen objects for processes spawned by this current session
 
+#--FIXME: staging files should be created with mkstemp, so that that they still exist as needed
+staging_files = [] #holds temporary files so they still exist for use by the subprocesses
+
+#path to the configuration file where the server stores its data between runs
+configfile = os.path.join(os.path.expanduser('~'), '.pvacseq_ui')
+
+#check the last system reboot, because it means that the logged pid's can now
+#be used by new processes
+reboot = subprocess.check_output(['last', 'reboot']).decode().split("\n")[0]
+
+#fetch data from the configuration file
+if os.path.isfile(configfile):
+    print("Resuming from saved state")
+    reader = open(configfile)
+    data = json.load(reader)
+    reader.close()
+    if data['reboot'] != reboot:
+        print("A reboot has occurred since the server was first started")
+        print(
+            "pid's of old pVAC-Seq runs with id's",
+            data['processid'],
+            "and lower may be innacurate"
+        )
+else:
+    print("No saved state found")
+    data={
+        'processid':-1,
+        'reboot': reboot
+    }
+
+#a mapping to provide a description of each result file based on its extension
 descriptions = {
     'chop.tsv':"Processed and filtered data, with peptide cleavage data added",
     'combined.parsed.tsv':"Processed data from IEDB, but with no filtering or extra data",
@@ -24,10 +53,14 @@ descriptions = {
     'tsv':"Raw input data parsed out of the input vcf"
 }
 
-def column_filter(id, column):
+def column_filter(column):
+    """standardize column names"""
     return column.replace(' ', '_').lower().strip()
 
 def gen_files_list(id):
+    """Generate the list of result files for a given process.  Stash them for later use"""
+    # if 'process-%d'%id not in data:
+    #     raise KeyError("The requested process (%d) does not exist"%id)
     if 'files' not in data['process-%d'%id]:
         data['process-%d'%id]['files'] = []
         base_dir = data['process-%d'%id]['output']
@@ -42,43 +75,90 @@ def gen_files_list(id):
         for path in sorted(os.listdir(base_dir)):
             if path.endswith('.tsv') and os.path.isfile(os.path.join(base_dir, path)):
                 data['process-%d'%id]['files'].append(os.path.join(base_dir, path))
+        savedata()
+
+def fetch_process(id):
+    """Fetch a tuple of available process info"""
+    return (
+        #This entry should always exist.  Any spawned process will have an entry in data
+        data['process-%d'%id] if 'process-%d'%id in data else {},
+        #This will only exist if spawned by the current run of the server
+        #If the server crashed and restarted, the child is orphaned,
+        #and the server will only be able to read its output files
+        #(unable to access the return code or terminate it)
+        children[id] if id in children else False
+    )
+
+def is_running(id):
+    """Returns True if the requested process looks like it's still running"""
+    process = fetch_process(id)
+    if not process[0]:
+        raise KeyError("The requested process (%d) does not exist"%id)
+    if process[1] and process[1].returncode == None:
+        return True
+    try:
+        #check if the process is active by sending a dummy signal
+        os.kill(process[0]['pid'], 0)
+    except ProcessLookupError:
+        return False
+    return True
+
+def savedata():
+    """Saves the data object to the configfile"""
+    writer = open(configfile, 'w')
+    json.dump(data, writer)
+    writer.close()
+
 
 def results_get(id):
-    processKey = 'process-%d'%id
-    if processKey in session and children[id][1].is_alive():
+    """Get the list of result files from a specific pVAC-Seq run"""
+    process = fetch_process(id)
+    if not process[0]:
+        raise KeyError("The requested process (%d) does not exist"%id)
+    if is_running(id):
         return []
-    output = []
     gen_files_list(id)
-    for fileID in range(len(data['process-%d'%id]['files'])):
-        extension = '.'.join(os.path.basename(data['process-%d'%id]['files'][fileID]).split('.')[1:])
+    output = []
+    for fileID in range(len(process[0]['files'])):
+        extension = '.'.join(os.path.basename(process[0]['files'][fileID]).split('.')[1:])
         output.append({
             'fileID':fileID,
             'description':descriptions[extension] if extension in descriptions else "Unknown file",
-            'display_name':os.path.relpath(data['process-%d'%id]['files'][fileID], data['process-%d'%id]['output']),
+            'display_name':os.path.relpath(process[0]['files'][fileID], process[0]['output']),
             'url':'/api/v1/processes/%d/results/%d'%(id, fileID),
-            'size':"%0.3f KB"%(os.path.getsize(os.path.join(data['process-%d'%id]['output'], data['process-%d'%id]['files'][fileID]))/1024)
+            'size':"%0.3f KB"%(os.path.getsize(os.path.join(process[0]['output'], process[0]['files'][fileID]))/1024)
         })
     return output
 
 def results_getfile(id, count = None, page = None, fileID = None):
-    processKey = 'process-%d'%id
-    if processKey in session and children[id][1].is_alive():
+    """Read data directly from a specific output file"""
+    process = fetch_process(id)
+    if not process[0]:
+        raise KeyError("The requested process (%d) does not exist"%id)
+    if is_running(id):
         return []
     gen_files_list(id)
-    raw_reader = open(data['process-%d'%id]['files'][fileID])
+    if fileID not in range(len(process[0]['files'])):
+        raise KeyError("The requested fileID (%d) does not exist for this process (%d)" %(fileID, id))
+    raw_reader = open(process[0]['files'][fileID])
     reader = csv.DictReader(raw_reader, delimiter='\t')
-    output = [{column_filter(id, k):entry[k] for k in entry} for entry in itertools.islice(reader, (page-1)*count, page*count)]
+    output = [{column_filter(k):entry[k] for k in entry} for entry in itertools.islice(reader, (page-1)*count, page*count)]
     raw_reader.close()
     return output
 
 def results_getcols(id, fileID):
-    processKey = 'process-%d'%id
-    if processKey in session and children[id][1].is_alive():
+    """Get a mapping of standardized column names -> original column names"""
+    process = fetch_process(id)
+    if not process[0]:
+        raise KeyError("The requested process (%d) does not exist"%id)
+    if is_running(id):
         return {}
     gen_files_list(id)
-    raw_reader = open(data['process-%d'%id]['files'][fileID])
+    if fileID not in range(len(process[0]['files'])):
+        raise KeyError("The requested fileID (%d) does not exist for this process (%d)" %(fileID, id))
+    raw_reader = open(process[0]['files'][fileID])
     reader = csv.DictReader(raw_reader, delimiter='\t')
-    output = {column_filter(id, field):field for field in reader.fieldnames}
+    output = {column_filter(field):field for field in reader.fieldnames}
     raw_reader.close()
     return output
 
@@ -87,6 +167,9 @@ def staging(input, samplename, alleles, epitope_lengths, prediction_algorithms,
           net_chop_method, netmhc_stab, top_result_per_mutation, top_score_metric,
           binding_threshold, minimum_fold_change, expn_val, net_chop_threshold,
           fasta_size, keep_tmp_files):
+    """Stage input for a new pVAC-Seq run.  Generate a unique output directory and \
+    save uploaded files to temporary locations (and give pVAC-Seq the filepaths). \
+    Then forward the command to start()"""
     staged_input = tempfile.NamedTemporaryFile('wb')
     input.save(staged_input)
     staged_input.flush()
@@ -121,6 +204,7 @@ def start(input, samplename, alleles, epitope_lengths, prediction_algorithms, ou
           net_chop_method, netmhc_stab, top_result_per_mutation, top_score_metric,
           binding_threshold, minimum_fold_change, expn_val, net_chop_threshold,
           fasta_size, keep_tmp_files):
+    """Build the command for pVAC-Seq, then spawn a new process to run it"""
     command = [
         input,
         samplename,
@@ -153,67 +237,92 @@ def start(input, samplename, alleles, epitope_lengths, prediction_algorithms, ou
         command.append('--top-result-per-mutation')
     if keep_tmp_files:
         command.append('-k')
-    logfile = tempfile.mkstemp(text=True) #generate a new file for logging
+
+    #stdout and stderr from the child process will be directed to this file
+    logfile = os.path.join(output, 'pVAC-Seq.log')
+
     data['processid']+=1
-    children[data['processid']] = mp.Process(target=_process_worker, args=(logfile[1], command), daemon=False)
-    children[data['processid']].start()
+    os.makedirs(os.path.dirname(logfile), exist_ok = True)
+    children[data['processid']] = subprocess.Popen(
+        [
+            sys.executable,
+            os.path.join(os.path.dirname(__file__), 'process_controller.py'),
+        ]+command,
+        stdout = open(logfile, 'w'),
+        stderr = subprocess.STDOUT,
+        #isolate the child in a new process group
+        #this way it will remainin running no matter what happens to this process
+        preexec_fn = os.setpgrp
+    )
+    #Store some data about the child process
     data['process-%d'%(data['processid'])] = {
         'command': "pvacseq run "+" ".join(command),
+        'logfile':logfile,
+        'pid':children[data['processid']].pid,
         'status': "Task Started",
-        'log': "",
-        'logfile':logfile[1],
         'output':os.path.abspath(output)
     }
+    savedata()
     return data['processid']
 
 def processes():
+    """Returns a list of processes, and whether or not each process is running"""
     return [{
         'id':i,
-        'running':children[i][1].is_alive()
-    } for i in range(len(children)) if 'process-%d'%i in session]
+        'running':is_running(i)
+    } for i in range(data['processid']+1) if 'process-%d'%i in data]
 
 def process_info(id):
-    processkey = 'process-%d'%id
-    reader = open(data[processkey]['logfile'])
-    data[processkey]['log'] = spinner.sub('', reader.read()).strip()
-    data[processkey]['status'] = data[processkey]['log'].split("\n")[-1]
-    running = False
-    if id not in children:
-        try:
-            os.waitpid()
+    """Returns more detailed information about a specific process"""
+    process = fetch_process(id)
+    if not process[0]:
+        raise KeyError("The requested process (%d) does not exist"%id)
+    reader = open(process[0]['logfile'])
+    log = spinner.sub('', reader.read()).strip()
+    process[0]['status'] = log.split("\n")[-1]
+    reader.close()
+    if process[1] and not is_running(id):
+        process[0]['status'] = "Process Complete: %d"%process[1].exitcode
+    savedata()
     return {
-        'pid':children[id][1].pid,
+        'pid':process[0]['pid'],
         'id':id,
-        'command':data[processkey]['command'],
-        'status':data[processkey]['status'],
-        'log':data[processkey]['log'],
-        'output':data[processkey]['output'],
-        'running':children[id][1].is_alive()
+        'attached': bool(process[1]),
+        'command':process[0]['command'],
+        'status':process[0]['status'],
+        'log':log,
+        'output':process[0]['output'],
+        'running':is_running(id)
     }
 
 def stop(id):
+    """Stops the requested process.  This is only allowed if the child is still attached"""
     status = process_info(id)
-    children[id][1].terminate()
-    # children[id][0].close()
+    if status['running'] and status['pid']>1:
+        children[id].terminate()
     return status
 
 def shutdown():
+    """Stops all attached, running children"""
     output = []
-    for i in range(len(children)):
-        if children[i][1].is_alive():
-            output.append(i)
-        children[i][1].join(.1)
-        if children[i][1].is_alive():
-            children[i][1].terminate()
+    for i in range(data['processid']+1):
+        if is_running(i) and i in children:
+            if children[i].returncode == None:
+                output.append(i)
+            children[i].wait(.1)
+            if children[i].returncode == None:
+                children[i].terminate()
     return output
 
 def test():
+    """Return the submission page (a stand-in until there is a proper ui for submission)"""
     reader = open("/Users/agrauber/Documents/pVAC-Seq/pvacseq/server/test_start.html")
     data = reader.read()
     reader.close()
     return data
 
 def check_allele(allele):
+    """Checks if the requested allele is supported by pVAC-Seq or not"""
     global allele_file
     if not allele_file:
         allele_file = tempfile.TemporaryFile('w+')
@@ -223,13 +332,3 @@ def check_allele(allele):
         if line.strip() == allele:
             return True
     return False
-
-def _process_worker(log_path, command):
-    from ...lib.main import main
-    # setup this process' stdout and stderr to write to the pipe's file descriptor
-    sys.stdout = open(log_path, mode='w+')
-    sys.stderr = sys.stdout
-    # run the pvacseq command
-    main(command)
-
-    sys.stdout.write("[[PROCESS COMPLETE]]")
