a7b58bc559c06fa73c408c4989b193c32064e55a
Susanna Kiwala <ssiebert@genome.wustl.edu>
2016-08-29 09:54:58 -0500

    Handle fasta sequences that are shorter than the epitope length

--- tests/test_generate_fasta.py
+++ tests/test_generate_fasta.py
@@ -309,6 +309,21 @@ class GenerateFastaTests(unittest.TestCase):
         expected_output_file = os.path.join(self.test_data_dir, 'output_X_sequence.fasta')
         self.assertTrue(cmp(generate_fasta_output_file.name, expected_output_file))
 
+    def test_input_file_with_resulting_short_fasta_sequence(self):
+        generate_fasta_input_file  = os.path.join(self.test_data_dir, 'input_short_fasta_sequence.tsv')
+        generate_fasta_output_file = tempfile.NamedTemporaryFile()
+
+        self.assertFalse(call([
+            self.python,
+            self.executable,
+            generate_fasta_input_file,
+            self.peptide_sequence_length,
+            self.epitope_length,
+            generate_fasta_output_file.name
+        ], shell=False))
+        expected_output_file = os.path.join(self.test_data_dir, 'output_short_fasta_sequence.fasta')
+        self.assertTrue(cmp(generate_fasta_output_file.name, expected_output_file))
+
     def test_distance_from_start_works_as_expected(self):
         sequence = 'KKLKILGMPFRNIRSILKMVN'
         position = 5
