3f3f7fab9ff659b31a2d93b30a9e03844c1b2c7d
Susanna Kiwala <ssiebert@genome.wustl.edu>
2016-08-30 09:58:49 -0500

    Add test for config_files

---
+++ tests/test_config_files.py
@@ -0,0 +1,25 @@
+import unittest
+import sys
+import os
+import py_compile
+from subprocess import check_output
+
+class ConfigFilesTests(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.python = sys.executable
+        base_dir = os.path.abspath(os.path.join(os.path.dirname(os.path.realpath(__file__)), '..'))
+        cls.executable_dir = os.path.join(base_dir, 'pvacseq', 'lib')
+        cls.executable     = os.path.join(cls.executable_dir, 'config_files.py')
+
+    def test_source_compiles(self):
+        self.assertTrue(py_compile.compile(self.executable))
+
+    def test_pvacseq_config_files_additional_input_file_list_command(self):
+        output = check_output(
+            [self.python, self.executable, 'additional_input_file_list'],
+            shell = False
+        )
+        expected_output = ("gene_expn_file: <genes.fpkm_tracking file from Cufflinks>\n"
+        + "transcript_expn_file: <isoforms.fpkm_tracking file from Cufflinks>\n")
+        self.assertEqual(output.decode(), expected_output)
