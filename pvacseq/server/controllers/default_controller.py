8269c1151c99405b5a9fe1fcb655c0bd9a970ee7
Susanna Kiwala <ssiebert@genome.wustl.edu>
2016-09-19 13:31:51 -0500

    Update API to current version of master

--- pvacseq/server/controllers/default_controller.py
+++ pvacseq/server/controllers/default_controller.py
@@ -7,6 +7,7 @@ import re
 import tempfile
 import json
 import shutil
+from yaml import dump
 
 spinner = re.compile(r'[\\\b\-/|]{2,}')
 allele_file = None
@@ -203,9 +204,13 @@ def results_getcols(id, fileID):
 
 def staging(input, samplename, alleles, epitope_lengths, prediction_algorithms,
           peptide_sequence_length, gene_expn_file, transcript_expn_file,
+          normal_snvs_coverage_file, normal_indels_coverage_file,
+          tdna_snvs_coverage_file, tdna_indels_coverage_file,
+          trna_snvs_coverage_file, trna_indels_coverage_file,
           net_chop_method, netmhc_stab, top_result_per_mutation, top_score_metric,
-          binding_threshold, minimum_fold_change, expn_val, net_chop_threshold,
-          fasta_size, keep_tmp_files):
+          binding_threshold, minimum_fold_change,
+          normal_cov, tdna_cov, trna_cov, normal_vaf, tdna_vaf, trna_vaf,
+          expn_val, net_chop_threshold, fasta_size, downstream_sequence_length, keep_tmp_files):
     """Stage input for a new pVAC-Seq run.  Generate a unique output directory and \
     save uploaded files to temporary locations (and give pVAC-Seq the filepaths). \
     Then forward the command to start()"""
@@ -223,27 +228,73 @@ def staging(input, samplename, alleles, epitope_lengths, prediction_algorithms,
     input.save(staged_input)
     staged_input.flush()
 
+    staged_additional_input_file_list = open(os.path.join(current_path, "Staging", "additional_input_file_list.yml"), 'w')
+
     staged_gene_expn_file = open(os.path.join(current_path, "Staging", "genes.fpkm_tracking"), 'wb')
     gene_expn_file.save(staged_gene_expn_file)
     staged_gene_expn_file.flush()
+    if staged_gene_expn_file.tell():
+        dump({"gene_expn_file": staged_gene_expn_file.name}, staged_additional_input_file_list, default_flow_style=False)
 
     staged_transcript_expn_file = open(os.path.join(current_path, "Staging", "transcript.fpkm_tracking"), 'wb')
     transcript_expn_file.save(staged_transcript_expn_file)
     staged_transcript_expn_file.flush()
+    if staged_transcript_expn_file.tell():
+        dump({"transcript_expn_file" : staged_transcript_expn_file.name}, staged_additional_input_file_list, default_flow_style=False)
+
+    staged_normal_snvs_coverage_file = open(os.path.join(current_path, "Staging", "normal_snvs.bam_readcount"), 'wb')
+    normal_snvs_coverage_file.save(staged_normal_snvs_coverage_file)
+    staged_normal_snvs_coverage_file.flush()
+    if staged_normal_snvs_coverage_file.tell():
+        dump({"normal_snvs_coverage_file" : staged_normal_snvs_coverage_file.name}, staged_additional_input_file_list, default_flow_style=False)
+
+    staged_normal_indels_coverage_file = open(os.path.join(current_path, "Staging", "normal_indels.bam_readcount"), 'wb')
+    normal_indels_coverage_file.save(staged_normal_indels_coverage_file)
+    staged_normal_indels_coverage_file.flush()
+    if staged_normal_indels_coverage_file.tell():
+        dump({"normal_indels_coverage_file" : staged_normal_indels_coverage_file.name}, staged_additional_input_file_list, default_flow_style=False)
+
+    staged_tdna_snvs_coverage_file = open(os.path.join(current_path, "Staging", "tdna_snvs.bam_readcount"), 'wb')
+    tdna_snvs_coverage_file.save(staged_tdna_snvs_coverage_file)
+    staged_tdna_snvs_coverage_file.flush()
+    if staged_tdna_snvs_coverage_file.tell():
+        dump({"tdna_snvs_coverage_file" : staged_tdna_snvs_coverage_file.name}, staged_additional_input_file_list, default_flow_style=False)
+
+    staged_tdna_indels_coverage_file = open(os.path.join(current_path, "Staging", "tdna_indels.bam_readcount"), 'wb')
+    tdna_indels_coverage_file.save(staged_tdna_indels_coverage_file)
+    staged_tdna_indels_coverage_file.flush()
+    if staged_tdna_indels_coverage_file.tell():
+        dump({"tdna_indels_coverage_file" : staged_tdna_indels_coverage_file.name}, staged_additional_input_file_list, default_flow_style=False)
+
+    staged_trna_snvs_coverage_file = open(os.path.join(current_path, "Staging", "trna_snvs.bam_readcount"), 'wb')
+    trna_snvs_coverage_file.save(staged_trna_snvs_coverage_file)
+    staged_trna_snvs_coverage_file.flush()
+    if staged_trna_snvs_coverage_file.tell():
+        dump({"trna_snvs_coverage_file" : staged_trna_snvs_coverage_file.name}, staged_additional_input_file_list, default_flow_style=False)
+
+    staged_trna_indels_coverage_file = open(os.path.join(current_path, "Staging", "trna_indels.bam_readcount"), 'wb')
+    trna_indels_coverage_file.save(staged_trna_indels_coverage_file)
+    staged_trna_indels_coverage_file.flush()
+    if staged_trna_indels_coverage_file.tell():
+        dump({"trna_indels_coverage_file" : staged_trna_indels_coverage_file.name}, staged_additional_input_file_list)
+    staged_additional_input_file_list.flush()
 
     return start(staged_input.name, samplename, alleles, epitope_lengths, prediction_algorithms, current_path,
-              peptide_sequence_length, staged_gene_expn_file.name if staged_gene_expn_file.tell() else "",
-              staged_transcript_expn_file.name if staged_transcript_expn_file.tell() else "", # check if any data written to file
+              peptide_sequence_length, staged_additional_input_file_list.name if staged_additional_input_file_list.tell() else "", # check if any data written to file
               net_chop_method, len(netmhc_stab), len(top_result_per_mutation), top_score_metric,
-              binding_threshold, minimum_fold_change, expn_val, net_chop_threshold,
-              fasta_size, len(keep_tmp_files))
+              binding_threshold, minimum_fold_change,
+              normal_cov, tdna_cov, trna_cov, normal_vaf, tdna_vaf, trna_vaf,
+              expn_val, net_chop_threshold,
+              fasta_size, downstream_sequence_length, len(keep_tmp_files))
 
 
 def start(input, samplename, alleles, epitope_lengths, prediction_algorithms, output,
-          peptide_sequence_length, gene_expn_file, transcript_expn_file,
+          peptide_sequence_length, additional_input_file_list,
           net_chop_method, netmhc_stab, top_result_per_mutation, top_score_metric,
-          binding_threshold, minimum_fold_change, expn_val, net_chop_threshold,
-          fasta_size, keep_tmp_files):
+          binding_threshold, minimum_fold_change,
+          normal_cov, tdna_cov, trna_cov, normal_vaf, tdna_vaf, trna_vaf,
+          expn_val, net_chop_threshold,
+          fasta_size, downstream_sequence_length, keep_tmp_files):
     """Build the command for pVAC-Seq, then spawn a new process to run it"""
     command = [
         'pvacseq',
@@ -261,13 +312,18 @@ def start(input, samplename, alleles, epitope_lengths, prediction_algorithms, ou
         '-m', top_score_metric,
         '-b', str(binding_threshold),
         '-c', str(minimum_fold_change),
+        '--normal-cov', str(normal_cov),
+        '--tdna-cov', str(tdna_cov),
+        '--trna-cov', str(trna_cov),
+        '--normal-vaf', str(normal_vaf),
+        '--tdna-vaf', str(tdna_vaf),
+        '--trna-vaf', str(trna_vaf),
         '--expn-val', str(expn_val),
-        '-s', str(fasta_size)
+        '-s', str(fasta_size),
+        '-d', str(downstream_sequence_length)
     ]
-    if len(gene_expn_file):
-        command += ['-g', gene_expn_file]
-    if len(transcript_expn_file):
-        command +=['-i', transcript_expn_file]
+    if len(additional_input_file_list):
+        command += ['-i', additional_input_file_list]
     if len(net_chop_method):
         command += [
             '--net-chop-method', net_chop_method,
