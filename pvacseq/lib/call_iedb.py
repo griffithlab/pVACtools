58450639cbeb804e59e535b87c85799ce9ef5188
Susanna Kiwala <ssiebert@genome.wustl.edu>
2016-11-29 10:33:00 -0600

    Use check=True to catch errors instead of checking returncode manually

--- pvacseq/lib/call_iedb.py
+++ pvacseq/lib/call_iedb.py
@@ -48,9 +48,7 @@ def main(args_input = sys.argv[1:]):
         sys.exit("Epitope length is required for class I binding predictions")
 
     if args.iedb_executable_path is not None:
-        response = run(prediction_class_object.iedb_executable_params(args), stdout=PIPE)
-        if response.returncode == 2:
-            sys.exit("Call to local IEDB install failed:\n%s\nAborting." % ' '.join(prediction_class_object.iedb_executable_params(args)))
+        response = run(prediction_class_object.iedb_executable_params(args), stdout=PIPE, check=True)
         response_text = response.stdout
         output_mode = 'wb'
     else:
