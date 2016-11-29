9a6ebc1843f10015db19cea08a8ac38b25dfc5f5
Susanna Kiwala <ssiebert@genome.wustl.edu>
2016-11-29 10:07:45 -0600

    Fix some formatting issues

--- pvacseq/lib/output_parser.py
+++ pvacseq/lib/output_parser.py
@@ -521,7 +521,7 @@ class DefaultOutputParser(OutputParser):
                             tsv_entry = tsv_entries[tsv_index]
                             key = "%s|%s" % (tsv_index, position)
                             if key not in iedb_results:
-                                iedb_results[key] = {}
+                                iedb_results[key]                      = {}
                                 iedb_results[key]['mt_scores']         = {}
                                 iedb_results[key]['mt_epitope_seq']    = epitope
                                 iedb_results[key]['gene_name']         = tsv_entry['gene_name']
@@ -572,7 +572,7 @@ class FusionOutputParser(OutputParser):
                         tsv_entry = tsv_entries[tsv_index]
                         key = "%s|%s" % (tsv_index, position)
                         if key not in iedb_results:
-                            iedb_results[key] = {}
+                            iedb_results[key]                      = {}
                             iedb_results[key]['mt_scores']         = {}
                             iedb_results[key]['wt_scores']         = {}
                             iedb_results[key]['mt_epitope_seq']    = epitope
