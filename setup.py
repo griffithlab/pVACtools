0a0071c84bd4a0bfac80e8b6e4511edbc4b91e76
Susanna Kiwala <ssiebert@genome.wustl.edu>
2017-09-05 08:55:50 -0500

    Update version

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ for dirpath, dirnames, filenames in os.walk("pvacseq/server"):
 
 setup(
     name="pvacseq",
-    version="4.0.9",
+    version="4.0.10",
     packages=["pvacseq", "pvacseq.lib", "pvacseq.server"],
     entry_points={
         "console_scripts":[
