1316c5347e6b1d51a1c2e5978763d0e5cea658a7
Susanna Kiwala <ssiebert@genome.wustl.edu>
2018-01-30 14:25:30 -0600

    Add option to provide tsv file with proximal variants
    Generate second mutant fasta sequence with those proximal variants (MTWPV) included

--- tests/test_fasta_generator.py
+++ tests/test_fasta_generator.py
@@ -526,6 +526,52 @@ class FastaGeneratorTests(unittest.TestCase):
         expected_key_output_file = os.path.join(self.test_data_dir, 'output_dnp.key')
         self.assertTrue(cmp(generate_fasta_key_output_file.name, expected_key_output_file))
 
+    def test_proximal_variants_generate_expected_file(self):
+        peptide_sequence_length        = '21'
+        generate_fasta_input_file      = os.path.join(self.test_data_dir, 'input_somatic_variant_with_proximal_variants.tsv')
+        generate_fasta_proximal_variants_file = os.path.join(self.test_data_dir, 'input_proximal_variants.tsv')
+        generate_fasta_output_file     = tempfile.NamedTemporaryFile()
+        generate_fasta_key_output_file = tempfile.NamedTemporaryFile()
+
+        generate_fasta_params = {
+            'input_file'                : generate_fasta_input_file,
+            'peptide_sequence_length'   : self.peptide_sequence_length,
+            'epitope_length'            : self.epitope_length,
+            'output_file'               : generate_fasta_output_file.name,
+            'output_key_file'           : generate_fasta_key_output_file.name,
+            'proximal_variants_file'    : generate_fasta_proximal_variants_file,
+        }
+        generator = FastaGenerator(**generate_fasta_params)
+
+        self.assertFalse(generator.execute())
+        expected_output_file = os.path.join(self.test_data_dir, 'output_proximal_variants.fasta')
+        self.assertTrue(cmp(generate_fasta_output_file.name, expected_output_file))
+        expected_key_output_file = os.path.join(self.test_data_dir, 'output_proximal_variants.key')
+        self.assertTrue(cmp(generate_fasta_key_output_file.name, expected_key_output_file))
+
+    def test_proximal_variants_for_inframe_insertion_generate_expected_file(self):
+        peptide_sequence_length        = '21'
+        generate_fasta_input_file      = os.path.join(self.test_data_dir, 'input_inframe_insertion_aa_replacement.tsv')
+        generate_fasta_proximal_variants_file = os.path.join(self.test_data_dir, 'input_proximal_variants.tsv')
+        generate_fasta_output_file     = tempfile.NamedTemporaryFile()
+        generate_fasta_key_output_file = tempfile.NamedTemporaryFile()
+
+        generate_fasta_params = {
+            'input_file'                : generate_fasta_input_file,
+            'peptide_sequence_length'   : self.peptide_sequence_length,
+            'epitope_length'            : self.epitope_length,
+            'output_file'               : generate_fasta_output_file.name,
+            'output_key_file'           : generate_fasta_key_output_file.name,
+            'proximal_variants_file'    : generate_fasta_proximal_variants_file,
+        }
+        generator = FastaGenerator(**generate_fasta_params)
+
+        self.assertFalse(generator.execute())
+        expected_output_file = os.path.join(self.test_data_dir, 'output_proximal_variants_inframe_insertion.fasta')
+        self.assertTrue(cmp(generate_fasta_output_file.name, expected_output_file))
+        expected_key_output_file = os.path.join(self.test_data_dir, 'output_proximal_variants_inframe_insertion.key')
+        self.assertTrue(cmp(generate_fasta_key_output_file.name, expected_key_output_file))
+
     def test_distance_from_start_works_as_expected(self):
         generate_fasta_input_file      = tempfile.NamedTemporaryFile()
         generate_fasta_output_file     = tempfile.NamedTemporaryFile()
