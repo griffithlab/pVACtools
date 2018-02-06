8d46633f8d66b4e3e43953dac12c26f81bd88067
Susanna Kiwala <ssiebert@genome.wustl.edu>
2018-02-06 09:17:45 -0600

    Add accessory-variants-file option to the main pipeline

--- tests/test_config_files.py
+++ tests/test_config_files.py
@@ -29,5 +29,6 @@ class ConfigFilesTests(unittest.TestCase):
             + "tdna_indels_coverage_file: <bam-readcount output file for tumor DNA BAM and indels>\n"
             + "trna_snvs_coverage_file: <bam-readcount output file for tumor RNA BAM and snvs>\n"
             + "trna_indels_coverage_file: <bam-readcount output file for tumor RNA BAM and indels>\n"
+            + "proximal_variants_file: <A TSV file with proximal variant information>\n"
         )
         self.assertEqual(output.decode(), expected_output)
