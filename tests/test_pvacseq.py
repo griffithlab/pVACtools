022c3499e4a889b6afed18af595162d3a7cba8c9
Susanna Kiwala <ssiebert@genome.wustl.edu>
2018-03-14 15:34:42 -0500

    Add phased VCF input to main pVACseq pipeline

--- tests/test_pvacseq.py
+++ tests/test_pvacseq.py
@@ -327,7 +327,41 @@ class PvacseqTests(unittest.TestCase):
         files,
         test_data_directory()
     )))
+    def test_pvacseq_pipeline_proximal_variants_vcf(self):
+        output_dir = tempfile.TemporaryDirectory()
+        additional_input_files = tempfile.NamedTemporaryFile('w')
+        additional_input_file_list = {
+            'phased_proximal_variants_vcf': os.path.join(self.test_data_directory, 'phased.vcf.gz'),
+        }
+        yaml.dump(additional_input_file_list, additional_input_files, default_flow_style=False)
 
+        params = [
+            os.path.join(self.test_data_directory, "input_somatic.vcf.gz"),
+            'Test',
+            'HLA-E*01:01',
+            'NetMHC',
+            output_dir.name,
+            '-e', '8',
+            '-i', additional_input_files.name,
+            '-s', '1000',
+            '-k',
+        ]
+        run.main(params)
+
+        for file_name in ['Test_21.fa.split_1-818', 'Test_21.fa.split_1-818.key']:
+            output_file   = os.path.join(output_dir.name, 'MHC_Class_I', 'tmp', file_name)
+            expected_file = os.path.join(self.test_data_directory, 'phased', 'MHC_Class_I', 'tmp', file_name)
+            self.assertTrue(cmp(output_file, expected_file, False))
+        for file_name in ['Test.proximal_variants.tsv', 'Test.all_epitopes.tsv', 'Test.filtered.tsv']:
+            output_file   = os.path.join(output_dir.name, 'MHC_Class_I', file_name)
+            expected_file = os.path.join(self.test_data_directory, 'phased', 'MHC_Class_I', file_name)
+            self.assertTrue(cmp(output_file, expected_file, False))
+
+    @patch('requests.post', unittest.mock.Mock(side_effect = lambda url, data, files=None: make_response(
+        data,
+        files,
+        test_data_directory()
+    )))
     def test_pvacseq_pipeline_sleep(self):
         output_dir_1 = tempfile.TemporaryDirectory()
         params_1 = [
