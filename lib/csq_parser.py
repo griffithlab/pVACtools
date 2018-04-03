e8413a58d0730850f12bff2149291a0c9895e545
Susanna Kiwala <ssiebert@genome.wustl.edu>
2018-04-03 10:04:54 -0500

    Move resolve_allele subroutine into csq_parser class

--- lib/csq_parser.py
+++ lib/csq_parser.py
@@ -19,3 +19,21 @@ class CsqParser:
                 transcripts.append(transcript)
 
         return transcripts
+
+    def resolve_alleles(self, entry):
+        alleles = {}
+        if entry.is_indel:
+            for alt in entry.ALT:
+                alt = str(alt)
+                if alt[0:1] != entry.REF[0:1]:
+                    alleles[alt] = alt
+                elif alt[1:] == "":
+                    alleles[alt] = '-'
+                else:
+                    alleles[alt] = alt[1:]
+        else:
+            for alt in entry.ALT:
+                alt = str(alt)
+                alleles[alt] = alt
+
+        return alleles
