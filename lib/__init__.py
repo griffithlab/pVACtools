4f8bd11f4b79bb5c4d23738240ad7a984b145ad3
Susanna Kiwala <ssiebert@genome.wustl.edu>
2018-04-13 15:12:53 -0500

    Create a condensed report

--- lib/__init__.py
+++ lib/__init__.py
@@ -2,6 +2,7 @@ __all__ = [
     "binding_filter",
     "call_iedb",
     "combine_parsed_outputs",
+    "condense_final_report",
     "input_file_converter",
     "download_example_data",
     "fasta_generator",
