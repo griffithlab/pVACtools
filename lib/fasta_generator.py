e555df8716040075a0091f30e2f34b67db3e0f44
Susanna Kiwala <ssiebert@genome.wustl.edu>
2018-04-18 11:58:55 -0500

    Skip amino acid changes with multiple stops that can't be resolved

--- lib/fasta_generator.py
+++ lib/fasta_generator.py
@@ -151,7 +151,11 @@ class FastaGenerator(metaclass=ABCMeta):
                 mutation_start_position, wildtype_subsequence = self.get_wildtype_subsequence(position, full_wildtype_sequence, wildtype_amino_acid_length, peptide_sequence_length, line)
                 mutation_end_position = mutation_start_position + wildtype_amino_acid_length
                 if wildtype_amino_acid != '-' and wildtype_amino_acid != wildtype_subsequence[mutation_start_position:mutation_end_position]:
-                    sys.exit("ERROR: There was a mismatch between the actual wildtype amino acid and the expected amino acid. Did you use the same reference build version for VEP that you used for creating the VCF?\n%s" % line)
+                    if line['amino_acid_change'].split('/')[0].count('*') > 1:
+                        print("Warning: Amino acid change is not sane - contains multiple stops. Skipping entry {}".format(line['index']))
+                        continue
+                    else:
+                        sys.exit("ERROR: There was a mismatch between the actual wildtype amino acid sequence ({}) and the expected amino acid sequence ({}). Did you use the same reference build version for VEP that you used for creating the VCF?\n{}".format(wildtype_subsequence[mutation_start_position:mutation_end_position], wildtype_amino_acid, line))
                 if stop_codon_added:
                     mutant_subsequence = wildtype_subsequence[:mutation_start_position] + mutant_amino_acid
                 else:
