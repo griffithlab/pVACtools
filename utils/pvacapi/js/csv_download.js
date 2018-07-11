30bcf42d143deffeae3cfed1bd272badb8f4da9d
Joshua McMichael <josh@jmcmichael.com>
2018-07-11 17:40:06 -0500

    properly creating CSV file

--- utils/pvacapi/js/csv_download.js
+++ utils/pvacapi/js/csv_download.js
@@ -1,27 +1,30 @@
 console.log('csv_download.js loaded.');
 var data = source.data;
-var filetext = 'name,income,years_experience\n';
-for (var i = 0; i < data['name'].length; i++) {
-    var currRow = [data['name'][i].toString(),
-                   data['salary'][i].toString(),
-                   data['years_experience'][i].toString().concat('\n')];
+var cols = source.column_names;
+var l = data[cols[0]].length; // grab the first col of data and count elements to get number of rows
+var csv = cols.join(',') + '\n';
 
-    var joined = currRow.join();
-    filetext = filetext.concat(joined);
+for (var i = 0; i < l; i++) {
+    var row = [];
+    cols.map(function(col) {
+        row.push(data[col][i]);
+    });
+
+    csv = csv.concat(row.join(',')) + '\n';
 }
 
 var filename = 'data_result.csv';
-var blob = new Blob([filetext], { type: 'text/csv;charset=utf-8;' });
+var blob = new Blob([csv], { type: 'text/csv;charset=utf-8;' });
 
 //addresses IE
-if (navigator.msSaveBlob) {
-    navigator.msSaveBlob(blob, filename);
-} else {
-    var link = document.createElement("a");
-    link = document.createElement('a')
-    link.href = URL.createObjectURL(blob);
-    link.download = filename
-    link.target = "_blank";
-    link.style.visibility = 'hidden';
-    link.dispatchEvent(new MouseEvent('click'))
-}
+// if (navigator.msSaveBlob) {
+//     navigator.msSaveBlob(blob, filename);
+// } else {
+//     var link = document.createElement("a");
+//     link = document.createElement('a')
+//     link.href = URL.createObjectURL(blob);
+//     link.download = filename
+//     link.target = "_blank";
+//     link.style.visibility = 'hidden';
+//     link.dispatchEvent(new MouseEvent('click'))
+// }
