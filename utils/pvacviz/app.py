d34ea0d7c8744df193147be31accd28b742062bc
Joshua McMichael <josh@jmcmichael.com>
2018-07-18 12:49:47 -0500

    switched to opening server in thread

--- utils/pvacviz/app.py
+++ utils/pvacviz/app.py
@@ -1,6 +1,8 @@
 from http.server import SimpleHTTPRequestHandler, HTTPServer
 from urllib.parse import urlparse
+import threading
 import os
+import webbrowser
 import time
 
 HOSTNAME = 'localhost'
@@ -32,13 +34,28 @@ def main():
 
     Handler = MyHandler
     httpd = HTTPServer((HOSTNAME, PORT), Handler)
-
-    print(time.asctime(), "Server Starts - %s:%s" % (HOSTNAME, PORT))
+    thread = threading.Thread(target=httpd.serve_forever)
 
     try:
-        httpd.serve_forever()
-    except KeyboardInterrupt:
+        print(time.asctime(), "%s:%s - Starting pVACviz client webserver" % (HOSTNAME, PORT))
+        thread.start()
+
+    except (KeyboardInterrupt, SystemExit):
+        print(time.asctime(), "%s:%s - Stopping pVACviz client webserver" % (HOSTNAME, PORT))
+        httpd.shutdown()
         pass
+
+    except OSError as err:
+        print("OS error while starting pVACviz client server: {0}".format(err))
+        httpd.shutdown()
+        pass
+
+    finally:
+        print(time.asctime(), "%s:%s - pVACviz server started." % (HOSTNAME, PORT))
+
+    # import rpdb; rpdb.set_trace()
+    print("Opening pVACviz client in default browser.")
+    webbrowser.open("%s:%s" % (HOSTNAME, PORT), new=1, autoraise=True)
 
 
 if __name__ == "__main__":
