094df48f8b2b746809dc234412cf8233abbb708c
Susanna Kiwala <ssiebert@genome.wustl.edu>
2018-08-24 11:44:17 -0500

    Change export to be a post request to /api/v1/processes/{processID}/export

--- tests/test_api.py
+++ tests/test_api.py
@@ -639,8 +639,8 @@ class APITests(unittest.TestCase):
             )
             self.assertEqual(response.status_code, 200, response.url+' : '+response.content.decode())
             self.assertIsInstance(response.json(), dict)
-        response = requests.get(
-            self.urlBase+'/export/%d'%processID,
+        response = requests.post(
+            self.urlBase+'/processes/%d/export'%processID,
             timeout = 5
         )
         self.assertEqual(response.status_code, 200, response.url+' : '+response.content.decode())
