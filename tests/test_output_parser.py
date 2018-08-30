cef5b1eb0202c23d171387d3a8301603d74026b1
Susanna Kiwala <ssiebert@genome.wustl.edu>
2018-08-30 13:34:15 -0500

    Add test_utils module to remove duplicate compare functions

--- tests/test_output_parser.py
+++ tests/test_output_parser.py
@@ -4,14 +4,7 @@ import sys
 import tempfile
 import py_compile
 from lib.output_parser import *
-
-def compare(path1, path2):
-    r1 = open(path1)
-    r2 = open(path2)
-    result = not len(set(r1.readlines())^set(r2.readlines()))
-    r1.close()
-    r2.close()
-    return result
+from .test_utils import *
 
 class OutputParserTests(unittest.TestCase):
     @classmethod
