f8a008a0e9d9b48d2ccc432f72185854ace101e7
Susanna Kiwala <ssiebert@genome.wustl.edu>
2018-08-30 13:34:15 -0500

    Add test_utils module to remove duplicate compare functions

--- tests/test_pvacfuse.py
+++ tests/test_pvacfuse.py
@@ -13,14 +13,7 @@ import lib
 import datetime
 from tools.pvacfuse import *
 from mock import patch
-
-def compare(path1, path2):
-    r1 = open(path1)
-    r2 = open(path2)
-    result = not len(set(r1.readlines())^set(r2.readlines()))
-    r1.close()
-    r2.close()
-    return result
+from .test_utils import *
 
 def make_response(data, files, path):
     if not files:
