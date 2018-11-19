b334b36c5eda497f54ed9bcea9c4353f2e9573b4
Susanna Kiwala <ssiebert@genome.wustl.edu>
2018-11-19 15:03:54 -0600

    Update version

--- setup.py
+++ setup.py
@@ -56,7 +56,7 @@ for dirpath, dirnames, filenames in os.walk("utils/pvacviz/client"):
 
 setup(
     name="pvactools",
-    version="1.1.2",
+    version="1.1.3",
     packages=[
         "tools",
         "tools.pvacfuse",
