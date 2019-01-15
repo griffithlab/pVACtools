03f9b21e5bb8da3a62880e2d0e52ddaefdc75e62
Susanna Kiwala <ssiebert@genome.wustl.edu>
2019-01-15 15:16:55 -0600

    Use eptiope_length to determine spacer wingspan length
    When creating the fasta file with all possible peptide-spacer-peptide
    combinations we were previously always using a wingspan of 10, which can
    result in epitopes that don't overlap the spacer for epitope lengths <=
    10.
    
    With this change we now write multiple fasta files, one for epitope
    length chosen by the user and use the appropriate wingspan length for
    each of them.
    
    When calling IEDB we then use the fasta file that matches the eptiope
    length for the specific call to IEDB and its epitope length.

--- tests/test_fasta_generator.py
+++ tests/test_fasta_generator.py
@@ -826,23 +826,21 @@ class FastaGeneratorTests(unittest.TestCase):
     def test_pvacvector_input_fasta_generates_expected_file(self):
         generate_fasta_input_file      = os.path.join(self.test_data_dir, 'pvacvector.fa')
         generate_fasta_output_file     = tempfile.NamedTemporaryFile()
-        generate_fasta_key_output_file = tempfile.NamedTemporaryFile()
 
         generate_fasta_params = {
             'input_file'                : generate_fasta_input_file,
             'peptide_sequence_length'   : self.peptide_sequence_length,
-            'epitope_length'            : self.epitope_length,
-            'output_file'               : generate_fasta_output_file.name,
-            'output_key_file'           : generate_fasta_key_output_file.name,
+            'epitope_lengths'           : [self.epitope_length],
+            'output_file_prefix'        : generate_fasta_output_file.name,
             'downstream_sequence_length': None,
         }
         generator = VectorFastaGenerator(**generate_fasta_params)
 
         self.assertFalse(generator.execute())
         expected_output_file = os.path.join(self.test_data_dir, 'output_pvacvector.fasta')
-        self.assertTrue(cmp(generate_fasta_output_file.name, expected_output_file))
+        self.assertTrue(cmp("{}.{}.tsv".format(generate_fasta_output_file.name, self.epitope_length), expected_output_file))
         expected_key_output_file = os.path.join(self.test_data_dir, 'output_pvacvector.key')
-        self.assertTrue(cmp(generate_fasta_key_output_file.name, expected_key_output_file))
+        self.assertTrue(cmp("{}.{}.tsv.key".format(generate_fasta_output_file.name, self.epitope_length), expected_key_output_file))
 
 
 
