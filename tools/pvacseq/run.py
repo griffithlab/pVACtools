60b110f1faa2717f8b85b09e15130afbef8a0a94
Susanna Kiwala <susanna.kiwala@wustl.edu>
2019-03-19 10:58:18 -0500

    Move the final status message to the PostProcessor

--- tools/pvacseq/run.py
+++ tools/pvacseq/run.py
@@ -48,7 +48,6 @@ def create_combined_reports(base_output_dir, args):
     post_processing_params['run_condense_report'] = True
 
     PostProcessor(**post_processing_params).execute()
-    print("\nDone: Pipeline finished successfully. File {} contains ranked list of filtered putative neoantigens for class I and class II predictions.\n".format(condensed_report_file))
 
 def main(args_input = sys.argv[1:]):
     parser = define_parser()
