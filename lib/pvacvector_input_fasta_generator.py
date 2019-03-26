f8fa98f8bff03281dedf521eda1973407ae9cce0
Susanna Kiwala <susanna.kiwala@wustl.edu>
2019-03-26 12:08:31 -0500

    If the start position is < 0 we need to reset it to 0 or the resulting sequence will be incorrect

--- lib/pvacvector_input_fasta_generator.py
+++ lib/pvacvector_input_fasta_generator.py
@@ -113,6 +113,8 @@ class PvacvectorInputFastaGenerator():
                 if end > len(full_sequence):
                     remainder = end - len(full_sequence)
                     start -= remainder
+                if start < 0:
+                    start = 0
                 extracted_sequence = full_sequence[int(start):int(end)]
             exists = False
             for other_index, other_extracted_sequence in extracted_peptides.items():
