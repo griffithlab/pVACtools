6cf61f76bf3b36ed11404b38d8cbba23a7217f87
Susanna Kiwala <susanna.kiwala@wustl.edu>
2019-03-29 11:33:18 -0500

    Update ranking of report to work with new set of score columns

--- lib/rank_epitopes.py
+++ lib/rank_epitopes.py
@@ -1,9 +1,10 @@
 import pandas as pd
 
 class RankEpitopes:
-    def __init__(self, input_file, output_file):
+    def __init__(self, input_file, output_file, top_score_metric):
         self.input_file = input_file
         self.output_file = output_file
+        self.top_score_metric = top_score_metric
 
     def headers(self):
         return [
@@ -14,9 +15,12 @@ class RankEpitopes:
             'HGVSp',
             'HLA Allele',
             'MT Epitope Seq',
-            'MT IC50',
-            'WT IC50',
-            'Fold Change',
+            'Median MT Score',
+            'Median WT Score',
+            'Median Fold Change',
+            'Best MT Score',
+            'Corresponding WT Score',
+            'Corresponding Fold Change',
             'Tumor DNA Depth',
             'Tumor DNA VAF',
             'Tumor RNA Depth',
@@ -26,9 +30,15 @@ class RankEpitopes:
         ]
 
     def execute(self):
+        if self.top_score_metric == 'median':
+            score_column = 'Median MT Score'
+            fold_change_column = 'Median Fold Change'
+        elif self.top_score_metric == 'lowest':
+            score_column = 'Best MT Score'
+            fold_change_column = 'Corresponding Fold Change'
         df = pd.read_csv(self.input_file, sep='\t', index_col=False)
-        df['mt_score_rank'] = df['MT IC50'].rank(numeric_only=True, ascending=False, method='dense').fillna(value=0.0)
-        df['fold_change_rank'] = df['Fold Change'].rank(numeric_only=True, ascending=True, method='dense').fillna(value=0.0)
+        df['mt_score_rank'] = df[score_column].rank(numeric_only=True, ascending=False, method='dense').fillna(value=0.0)
+        df['fold_change_rank'] = df[fold_change_column].rank(numeric_only=True, ascending=True, method='dense').fillna(value=0.0)
         df['mt_allele_exp'] = df['Tumor RNA VAF'] * df['Gene Expression']
         df['mt_allele_exp_rank'] = df['mt_allele_exp'].rank(numeric_only=True, ascending=True, method='dense').fillna(value=0.0)
         df['tumor_dna_vaf_rank'] = df['Tumor DNA VAF'].rank(numeric_only=True, ascending=True, method='dense').fillna(value=0.0)
