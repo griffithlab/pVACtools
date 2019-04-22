4fdd5b6a109976bfd77c65597e9c3646ce63eaf3
Susanna Kiwala <susanna.kiwala@wustl.edu>
2019-04-22 11:55:30 -0500

    Upgrade to version 1.3.6

--- setup.py
+++ setup.py
@@ -56,7 +56,7 @@ for dirpath, dirnames, filenames in os.walk("utils/pvacviz/client"):
 
 setup(
     name="pvactools",
-    version="1.3.5",
+    version="1.3.6",
     packages=[
         "tools",
         "tools.pvacfuse",
