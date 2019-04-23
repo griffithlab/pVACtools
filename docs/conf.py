0afb417601cbf58cdbf20ba0cfd533e43577cd24
Susanna Kiwala <susanna.kiwala@wustl.edu>
2019-04-23 12:10:33 -0500

    Switch to RTD theme until basicstrap is fixed for latest Sphinx version

--- docs/conf.py
+++ docs/conf.py
@@ -34,7 +34,7 @@ sys.path.insert(0, os.path.abspath('../tools'))
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinxjp.themes.basicstrap',
+    #'sphinxjp.themes.basicstrap',
     'sphinx.ext.autodoc',
     'sphinx.ext.coverage',
     #'sphinxarg.ext',
@@ -129,18 +129,18 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'basicstrap'
-#html_theme = 'classic'
+#html_theme = 'basicstrap'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
 html_theme_options = {
-    'header_inverse': False,
-    'relbar_inverse': False,
-    'inner_theme': True,
-    'inner_theme_name': 'bootswatch-cosmo',
+    #'header_inverse': False,
+    #'relbar_inverse': False,
+    #'inner_theme': True,
+    #'inner_theme_name': 'bootswatch-cosmo',
 }
 
 # Add any paths that contain custom themes here, relative to this directory.
