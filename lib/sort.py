138e2e68a416a390dc4a62f4fd461555f657fb39
Susanna Kiwala <susanna.kiwala@wustl.edu>
2019-08-06 12:15:21 -0500

    Fix bugs in top score filter logic
    - Bug in the filter method for NA expression values
    - Handling of the ensembl ID for pVACfuse where the transcript might be
    a combination
    - Sorting of pVACbind to also use the Mutation column
    - Tests should use the filecmp.cmp method to ensure order of output
    files are correct after sorting

--- lib/sort.py
+++ lib/sort.py
@@ -11,11 +11,13 @@ def default_sort(rows, top_score_metric):
 
 def pvacbind_sort(rows, top_score_metric):
     if top_score_metric == 'median':
-        sorted_rows = sorted(rows, key=lambda row: (float(row['Best Score'])))
+        sorted_rows = sorted(rows, key=lambda row: (row['Mutation']))
+        sorted_rows = sorted(sorted_rows, key=lambda row: (float(row['Best Score'])))
         sorted_rows = sorted(sorted_rows, key=lambda row: (int(row['Sub-peptide Position'])))
         sorted_rows = sorted(sorted_rows, key=lambda row: (float(row['Median Score'])))
     elif top_score_metric == 'lowest':
-        sorted_rows = sorted(rows, key=lambda row: (float(row['Median Score'])))
+        sorted_rows = sorted(rows, key=lambda row: (row['Mutation']))
+        sorted_rows = sorted(sorted_rows, key=lambda row: (float(row['Median Score'])))
         sorted_rows = sorted(sorted_rows, key=lambda row: (int(row['Sub-peptide Position'])))
         sorted_rows = sorted(sorted_rows, key=lambda row: (float(row['Best Score'])))
     return sorted_rows
