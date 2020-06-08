865439abe2ad4770be85805037e566b38b09d030
Susanna Kiwala <susanna.kiwala@wustl.edu>
2020-06-08 09:25:14 -0500

    Upgrade to version 1.5.9

--- setup.py
+++ setup.py
@@ -61,7 +61,7 @@ for dirpath, dirnames, filenames in os.walk("utils/pvacviz/client"):
 
 setup(
     name="pvactools",
-    version="1.5.8",
+    version="1.5.9",
     packages=[
         "tools",
         "tools.pvacbind",
