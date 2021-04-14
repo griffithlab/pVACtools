51eaa728c0ad72d77494ef71dff47222656e8395
Susanna Kiwala <susanna.kiwala@wustl.edu>
2021-04-14 09:20:31 -0500

    Update pvacfuse generate_protein_fasta tests

--- tests/test_pvacfuse_generate_protein_fasta.py
+++ tests/test_pvacfuse_generate_protein_fasta.py
@@ -20,24 +20,6 @@ class GenerateFastaTests(unittest.TestCase):
     def test_source_compiles(self):
         self.assertTrue(py_compile.compile(self.executable))
 
-    def test_integrate_input_file_generates_expected_file(self):
-        generate_protein_fasta_input_file  = os.path.join(self.test_data_dir, 'fusions_annotated.bedpe')
-        generate_protein_fasta_output_file = tempfile.NamedTemporaryFile()
-        generate_protein_fasta_output_tsv = "{}.manufacturability.tsv".format(generate_protein_fasta_output_file.name)
-
-        self.assertFalse(call([
-            self.python,
-            self.executable,
-            generate_protein_fasta_input_file,
-            self.flanking_sequence_length,
-            generate_protein_fasta_output_file.name,
-            '-d', 'full',
-        ], shell=False))
-        expected_output_file = os.path.join(self.test_data_dir, 'output.fasta')
-        self.assertTrue(cmp(generate_protein_fasta_output_file.name, expected_output_file))
-        expected_tsv_file = os.path.join(self.test_data_dir, 'output.tsv')
-        self.assertTrue(cmp(generate_protein_fasta_output_tsv, expected_tsv_file))
-
     def test_agfusion_input_file_generates_expected_file(self):
         generate_protein_fasta_input_file  = os.path.join(self.test_data_dir, 'agfusion')
         generate_protein_fasta_output_file = tempfile.NamedTemporaryFile()
@@ -54,7 +36,7 @@ class GenerateFastaTests(unittest.TestCase):
         self.assertTrue(cmp(generate_protein_fasta_output_file.name, expected_output_file))
 
     def test_input_tsv(self):
-        generate_protein_fasta_input_file  = os.path.join(self.test_data_dir, 'fusions_annotated.bedpe')
+        generate_protein_fasta_input_file  = os.path.join(self.test_data_dir, 'agfusion')
         generate_protein_fasta_input_tsv   = os.path.join(self.test_data_dir, 'input.tsv')
         generate_protein_fasta_output_file = tempfile.NamedTemporaryFile()
 
