3b310c4916b918eebc827a505cc8245dbc259008
Susanna Kiwala <susanna.kiwala@wustl.edu>
2021-08-24 13:23:18 -0500

    Handle various error states from NetMHCstabpan server

--- lib/netmhc_stab.py
+++ lib/netmhc_stab.py
@@ -7,9 +7,8 @@ import re
 import os
 from time import sleep
 import lib.run_utils
-
-cycle = ['|', '/', '-', '\\']
-methods = ['cterm', '20s']
+import random
+import logging
 
 def main(args_input = sys.argv[1:]):
     parser = argparse.ArgumentParser("pvacseq net_chop", formatter_class=argparse.ArgumentDefaultsHelpFormatter)
@@ -26,7 +25,9 @@ def main(args_input = sys.argv[1:]):
     args = parser.parse_args(args_input)
     jobid_searcher = re.compile(r'<!-- jobid: [0-9a-fA-F]*? status: (queued|active)')
     result_delimiter = re.compile(r'-{20,}')
-    fail_searcher = re.compile(r'(Failed run|Problematic input:)')
+    fail_searcher = re.compile(r'(Failed run|Problematic input:|Configuration error)')
+    rejected_searcher = re.compile(r'status: rejected')
+    success_searcher = re.compile(r'# NetMHCstabpan version 1.0')
     allele_searcher = re.compile(r'^(.*?) : Distance to trai?ning data .*? nearest neighbor (.*?)\)$', re.MULTILINE)
     reader = csv.DictReader(args.input_file, delimiter='\t')
     writer = csv.DictWriter(
@@ -37,9 +38,6 @@ def main(args_input = sys.argv[1:]):
     )
     writer.writeheader()
     x = 0
-    i=1
-    print("Waiting for results from NetMHCStabPan... |", end='')
-    sys.stdout.flush()
     for chunk in lib.run_utils.split_file(reader, 100):
         peptide_lengths = set()
         staging_file = tempfile.NamedTemporaryFile(mode='w+')
@@ -60,60 +58,72 @@ def main(args_input = sys.argv[1:]):
         staging_file.seek(0)
         allele_list = [allele.replace('*', '') for allele in alleles_in_chunk]
         allele_list.sort()
-        response = requests.post(
-            "https://services.healthtech.dtu.dk/cgi-bin/webface2.cgi",
-            files={'SEQSUB':(staging_file.name, staging_file, 'text/plain')},
-            data = {
-                'configfile':'/var/www/html/services/NetMHCstabpan-1.0/webface.cf',
-                'inp':'0',
-                'len': ','.join(peptide_lengths),
-                'master':'1',
-                'slave0':allele_list[-1],
-                'allele':','.join(allele_list),
-                'thrs':'0.5',
-                'thrw': '2',
-                'incaff': '0',
-                'sort1':'-1',
-                'waff':'0.8',
-                'sort2':'-1'
-            }
-        )
-        while jobid_searcher.search(response.content.decode()):
-            for _ in range(10):
-                sys.stdout.write('\b'+cycle[i%4])
-                sys.stdout.flush()
-                sleep(1)
-                i+=1
-            response = requests.get(response.url)
+        response = query_netmhcstabpan_server(staging_file, peptide_lengths, allele_list, jobid_searcher)
+
         if fail_searcher.search(response.content.decode()):
-            sys.stdout.write('\b\b')
-            print('Failed!')
-            print("NetMHCStabPan encountered an error during processing")
-            sys.exit(1)
-        pending = []
-        allele_map = {item[0]:item[1] for item in allele_searcher.findall(response.content.decode())}
-        results = [item.strip() for item in result_delimiter.split(response.content.decode())]
-        for i in range(2, len(results), 4): #examine only the parts we want, skipping all else
-            for line in results[i].split('\n'):
-                data = [word for word in line.strip().split(' ') if len(word)]
-                line = current_buffer[data[3]]
-                if 'Epitope Seq' in line:
-                    length = len(line['Epitope Seq'])
-                else:
-                    length = len(line['MT Epitope Seq'])
-                if data[1] == line['HLA Allele'] and len(data[2]) == length:
-                    line.update({
-                        'Predicted Stability':data[4],
-                        'Half Life':data[5],
-                        'Stability Rank':data[6],
-                        'NetMHCstab allele':allele_map[line['HLA Allele'].replace('*', '', 1)]
-                    })
-                    pending.append([int(data[3], 16), {k:line[k] for k in line}])
-        writer.writerows([{k:entry[1][k] for k in entry[1]} for entry in sorted(pending, key=lambda x:x[0])])
-    sys.stdout.write('\b\b')
-    print("OK")
+            raise Exception("NetMHCstabpan encountered an error during processing.\n{}".format(response.content.decode()))
+
+        while rejected_searcher.search(response.content.decode()):
+            logging.warning("Too many jobs submitted to NetMHCstabpan server. Waiting to retry.")
+            sleep(random.randint(5, 10))
+            staging_file.seek(0)
+            response = query_netmhcstabpan_server(staging_file, peptide_lengths, allele_list, jobid_searcher)
+
+        if success_searcher.search(response.content.decode()):
+            pending = []
+            allele_map = {item[0]:item[1] for item in allele_searcher.findall(response.content.decode())}
+            results = [item.strip() for item in result_delimiter.split(response.content.decode())]
+            for i in range(2, len(results), 4): #examine only the parts we want, skipping all else
+                for line in results[i].split('\n'):
+                    data = [word for word in line.strip().split(' ') if len(word)]
+                    line = current_buffer[data[3]]
+                    if 'Epitope Seq' in line:
+                        length = len(line['Epitope Seq'])
+                    else:
+                        length = len(line['MT Epitope Seq'])
+                    if data[1] == line['HLA Allele'] and len(data[2]) == length:
+                        line.update({
+                            'Predicted Stability':data[4],
+                            'Half Life':data[5],
+                            'Stability Rank':data[6],
+                            'NetMHCstab allele':allele_map[line['HLA Allele'].replace('*', '', 1)]
+                        })
+                        pending.append([int(data[3], 16), {k:line[k] for k in line}])
+            if len(pending) == 0:
+                raise Exception("Unexpected return value from NetMHCstabpan server. Unable to parse response.\n{}".format(response.content.decode()))
+            writer.writerows([{k:entry[1][k] for k in entry[1]} for entry in sorted(pending, key=lambda x:x[0])])
+        else:
+            raise Exception("Unexpected return value from NetMHCstabpan server. Unable to parse response.\n{}".format(response.content.decode()))
     args.output_file.close()
     args.input_file.close()
+
+def query_netmhcstabpan_server(staging_file, peptide_lengths, allele_list, jobid_searcher):
+    response = requests.post(
+        "https://services.healthtech.dtu.dk/cgi-bin/webface2.cgi",
+        files={'SEQSUB':(staging_file.name, staging_file, 'text/plain')},
+        data = {
+            'configfile':'/var/www/html/services/NetMHCstabpan-1.0/webface.cf',
+            'inp':'0',
+            'len': ','.join(peptide_lengths),
+            'master':'1',
+            'slave0':allele_list[-1],
+            'allele':','.join(allele_list),
+            'thrs':'0.5',
+            'thrw': '2',
+            'incaff': '0',
+            'sort1':'-1',
+            'waff':'0.8',
+            'sort2':'-1',
+        }
+    )
+    if response.status_code != 200:
+        raise Exception("Error posting request to NetMHCstabpan server.\n{}".format(response.content.decode()))
+    while jobid_searcher.search(response.content.decode()):
+        sleep(10)
+        response = requests.get(response.url)
+        if response.status_code != 200:
+            raise Exception("Error posting request to NetMHCstabpan server.\n{}".format(response.content.decode()))
+    return response
 
 
 if __name__ == '__main__':
