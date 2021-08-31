edf541e9609a65e7774b96e79f2e7c68947dbcda
Susanna Kiwala <susanna.kiwala@wustl.edu>
2021-08-31 14:44:37 -0500

    Don't fail if www directory already exists

--- lib/aggregate_all_epitopes.py
+++ lib/aggregate_all_epitopes.py
@@ -433,7 +433,7 @@ class PvacseqAggregateAllEpitopes(AggregateAllEpitopes, metaclass=ABCMeta):
         module_dir = os.path.dirname(__file__)
         r_folder = os.path.abspath(os.path.join(module_dir,"..","tools","pvacview"))
         destination = os.path.abspath(os.path.dirname(self.output_file))
-        os.mkdir(os.path.join(destination, "www"))
+        os.makedirs(os.path.join(destination, "www"), exist_ok=True)
         for i in ["ui.R", "app.R", "server.R", "styling.R", "anchor_and_helper_functions.R"]:
             shutil.copy(os.path.join(r_folder, i), os.path.join(destination, i))
         for i in ["anchor.png", "pVACview_logo.png", "pVACview_logo_mini.png"]:
