1d7174fe5cd1965384e41818bc224db5c41e90d4
Susanna Kiwala <susanna.kiwala@wustl.edu>
2021-09-07 11:14:28 -0500

    Remove stray test file copy commands

--- tests/test_pvacfuse.py
+++ tests/test_pvacfuse.py
@@ -190,9 +190,6 @@ class PvacfuseTests(unittest.TestCase):
             ):
                 output_file   = os.path.join(output_dir.name, 'MHC_Class_I', file_name)
                 expected_file = os.path.join(self.test_data_directory, 'fusions', 'MHC_Class_I', file_name.replace('sample.name', 'Test'))
-                if file_name == 'sample.name.filtered.tsv':
-                    import shutil
-                    shutil.copy(output_file, os.path.join(self.pVac_directory, '..', '..', '.temp_backup', 'pvacfuse', 'testncoutfile', 'gend.Test.filtered.tsv'))
                 self.assertTrue(compare(output_file, expected_file),  "files don't match %s - %s" %(output_file, expected_file))
 
             for file_name in (
