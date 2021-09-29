46324b282c95dc07d618fa3490e1cda34fdf39b7
Susanna Kiwala <susanna.kiwala@wustl.edu>
2021-09-29 15:37:50 -0500

    Remove fusion modules that are no longer needed

--- tests/test_output_parser.py
+++ tests/test_output_parser.py
@@ -4,7 +4,7 @@ import sys
 import tempfile
 import py_compile
 
-from pvactools.lib.output_parser import DefaultOutputParser, FusionOutputParser, UnmatchedSequencesOutputParser
+from pvactools.lib.output_parser import DefaultOutputParser, UnmatchedSequencesOutputParser
 from .test_utils import *
 
 class OutputParserTests(unittest.TestCase):
@@ -341,25 +341,6 @@ class OutputParserTests(unittest.TestCase):
         }
         parser = DefaultOutputParser(**parse_output_params)
         self.assertFalse(parser.execute())
-
-    def test_parse_output_runs_and_produces_expected_output_for_fusions(self):
-        parse_output_input_iedb_file = [os.path.join(self.test_data_dir, "input_fusions.ann.HLA-A*29:02.9.tsv")]
-        parse_output_input_tsv_file = os.path.join(self.test_data_dir, "input_fusions.tsv")
-        parse_output_key_file = os.path.join(self.test_data_dir, "input_fusions.key")
-        parse_output_output_file = tempfile.NamedTemporaryFile()
-
-        parse_output_params = {
-            'input_iedb_files'       : parse_output_input_iedb_file,
-            'input_tsv_file'         : parse_output_input_tsv_file,
-            'key_file'               : parse_output_key_file,
-            'output_file'            : parse_output_output_file.name,
-            'sample_name'            : 'input_fusions',
-        }
-        parser = FusionOutputParser(**parse_output_params)
-
-        self.assertFalse(parser.execute())
-        expected_output_file  = os.path.join(self.test_data_dir, "output_fusions.iedb.parsed.tsv")
-        self.assertTrue(compare(parse_output_output_file.name, expected_output_file))
 
     def test_parse_output_runs_and_produces_expected_output_for_pvacvector(self):
         parse_output_input_iedb_file = [os.path.join(self.test_data_dir, "input_pvacvector.ann.H-2-Kb.8.tsv")]
