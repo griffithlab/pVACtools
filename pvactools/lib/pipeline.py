4b34ca2131b3b79d4597e8807298d5f41cc6ab90
Megan Richters <meganrichters4@gmail.com>
2022-01-23 18:08:05 -0600

    modifying files to run PvacsplicePipeline()

--- pvactools/lib/pipeline.py
+++ pvactools/lib/pipeline.py
@@ -20,7 +20,7 @@ import logging
 from pvactools.lib.prediction_class import *
 from pvactools.lib.input_file_converter import VcfConverter
 from pvactools.lib.fasta_generator import FastaGenerator, VectorFastaGenerator
-from pvactools.lib.output_parser import DefaultOutputParser, UnmatchedSequencesOutputParser
+from pvactools.lib.output_parser import DefaultOutputParser, UnmatchedSequencesOutputParser, PvacspliceOutputParser
 from pvactools.lib.post_processor import PostProcessor
 import pvactools.lib.call_iedb
 import pvactools.lib.combine_parsed_outputs
@@ -73,6 +73,7 @@ class Pipeline(metaclass=ABCMeta):
         self.blastp_db                   = kwargs.pop('blastp_db', 'refseq_select_prot')
         self.run_post_processor          = kwargs.pop('run_post_processor', True)
         self.proximal_variants_file      = None
+        self.annotated_vcf               = kwargs.pop('annotated_vcf', None)
         tmp_dir = os.path.join(self.output_dir, 'tmp')
         os.makedirs(tmp_dir, exist_ok=True)
         self.tmp_dir = tmp_dir
@@ -154,6 +155,7 @@ class Pipeline(metaclass=ABCMeta):
             'vcf'  : 'DefaultOutputParser',
             'pvacvector_input_fasta': 'UnmatchedSequencesOutputParser',
             'fasta': 'UnmatchedSequencesOutputParser',
+            'pvacsplice_vcf_fasta' : 'PvacspliceOutputParser',
         }
         parser_type = parser_types[self.input_file_type]
         parser = getattr(sys.modules[__name__], parser_type)
@@ -740,6 +742,56 @@ class PvacbindPipeline(Pipeline):
     def execute(self):
         self.print_log()
 
+        split_parsed_output_files = []
+        for length in self.epitope_lengths:
+            self.create_per_length_fasta_and_process_stops(length)
+            chunks = self.split_fasta_file(length)
+            self.call_iedb(chunks, length)
+            split_parsed_output_files.extend(self.parse_outputs(chunks, length))
+
+        if len(split_parsed_output_files) == 0:
+            status_message("No output files were created. Aborting.")
+            return
+
+        self.combined_parsed_outputs(split_parsed_output_files)
+
+        if not self.run_post_processor:
+            return
+
+        post_processing_params = copy.copy(vars(self))
+        post_processing_params['input_file'] = self.combined_parsed_path()
+        post_processing_params['file_type'] = 'pVACbind'
+        post_processing_params['filtered_report_file'] = self.final_path()
+        post_processing_params['run_coverage_filter'] = False
+        post_processing_params['run_transcript_support_level_filter'] = False
+        post_processing_params['minimum_fold_change'] = None
+        post_processing_params['run_manufacturability_metrics'] = True
+        post_processing_params['fasta'] = self.input_file
+        if self.net_chop_method:
+            post_processing_params['net_chop_fasta'] = self.net_chop_fasta
+            post_processing_params['run_net_chop'] = True
+        else:
+            post_processing_params['run_net_chop'] = False
+        if self.netmhc_stab:
+            post_processing_params['run_netmhc_stab'] = True
+        else:
+            post_processing_params['run_netmhc_stab'] = False
+        PostProcessor(**post_processing_params).execute()
+
+        if self.keep_tmp_files is False:
+            shutil.rmtree(self.tmp_dir)
+
+class PvacsplicePipeline(PvacbindPipeline):
+    def execute(self):
+        self.print_log()
+        self.convert_vcf() # ends with: writing sample_name.tsv
+
+        total_row_count = self.tsv_entry_count() # counts sample_name.tsv
+        if total_row_count == 0:
+            print("The TSV file is empty. Please check that the input VCF contains missense, inframe indel, or frameshift mutations.")
+            return
+        self.split_tsv_file(total_row_count) # splits sample_name.tsv
+        
         split_parsed_output_files = []
         for length in self.epitope_lengths:
             self.create_per_length_fasta_and_process_stops(length)
