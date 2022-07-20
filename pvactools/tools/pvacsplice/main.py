88474a2ce80333990851e0d5fd4b3e12daac7033
Megan Richters <meganrichters4@gmail.com>
2022-07-20 08:48:25 -0500

    reformat parser to run main program

--- pvactools/tools/pvacsplice/main.py
+++ pvactools/tools/pvacsplice/main.py
@@ -3,27 +3,30 @@ import sys
 from subprocess import call
 import os
 import pkg_resources
-from tools.pvacsplice import *
+from pvactools.tools.pvacsplice import *
 
-def main():
+def define_parser():
     parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     subparsers = parser.add_subparsers()
 
-    #add subcommand
+    #add subcommands
     run_main_program_parser = subparsers.add_parser(
         "run",
-        help="Run the pVACsplice pipeline",
+        help="Run the pVACsplice pipeline.",
         add_help=False
     )
     run_main_program_parser.set_defaults(func=run)
 
+def main():
+    parser = define_parser()
     args = parser.parse_known_args()
     try:
         args[0].func.main(args[1])
     except AttributeError as e:
         parser.print_help()
         print("Error: No command specified")
-        raise
+        sys.exit(-1)
+
 
 if __name__ == '__main__':
     main()
