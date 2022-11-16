15811097ab1b1a3511dfb39c48a8d3d3d287eb4d
Megan Richters <meganrichters4@gmail.com>
2022-11-16 10:51:29 -0600

    added support for input gtf file and reporting frameshift events in the final all_epitopes file

--- pvactools/lib/splice_pipeline.py
+++ pvactools/lib/splice_pipeline.py
@@ -2,6 +2,7 @@ import os
 import pandas as pd
 import pyfaidx
 import shutil
+from gtfparse import read_gtf
 from pvactools.lib.filter_regtools_results import *
 from pvactools.lib.junction_to_fasta import *
 from pvactools.lib.fasta_to_kmers import *
@@ -17,15 +18,16 @@ class JunctionPipeline():
         self.output_dir              = kwargs['base_output_dir']
         self.fasta_path              = kwargs['ref_fasta']        
         self.annotated_vcf           = kwargs['annotated_vcf']
-        self.input_gtf              = 'insert_cluster_gtf_path'
+        self.gtf_file                = kwargs['gtf_file']
         self.class_i_epitope_length  = kwargs['class_i_epitope_length']
         self.class_ii_epitope_length = kwargs['class_ii_epitope_length']
         self.class_i_hla             = kwargs['class_i_hla']
         self.class_ii_hla            = kwargs['class_ii_hla']
-        self.junction_score          = kwargs.pop('junction_score', 10)
-        self.variant_distance        = kwargs.pop('variant_distance', 100)
-        self.maximum_transcript_support_level = kwargs.pop('maximum_transcript_support_level', None)
+        self.junction_score          = kwargs['junction_score']
+        self.variant_distance        = kwargs['variant_distance']
+        self.tsl                     = kwargs['maximum_transcript_support_level']
         self.normal_sample_name      = kwargs.pop('normal_sample_name', None)
+        self.gtf_data = self.load_gtf_df()
     
     def execute(self):
         self.filter_regtools_results()
@@ -34,6 +36,38 @@ class JunctionPipeline():
         self.combine_inputs()
         self.junction_to_fasta()
         self.fasta_to_kmers()
+
+    def load_gtf_df(self):
+        print('Converting gtf file to dataframe')
+
+        # this worked in VScode interactive python but not in the pvacsplice docker?? #
+
+        # mem: 1.25 GB
+        # gtf_df_all = read_gtf(self.gtf_file, usecols=['feature', 'seqname', 'start', 'end', 'transcript_id', 'transcript_biotype', 'transcript_version', 'transcript_support_level', 'exon_number', 'gene_name', 'gene_id'])
+
+        # print('GTF all df')
+        # print(gtf_df_all['feature'].unique())
+        # print(gtf_df_all.dtypes)
+
+        # # mem: 0.68 GB 
+        # # tscript and CDS - this is coding coordinates exon by exon (leaving out 5/3' UTRs in exon body)
+        # gtf_df = gtf_df_all.loc[(gtf_df_all['feature'].isin(['CDS', 'transcript'])) & (gtf_df_all['transcript_support_level'] == '1') & (gtf_df_all['transcript_biotype'] == 'protein_coding')].replace(["^\s*$"], 'NA', regex=True)
+        
+        # print('GTF feaure df')
+        # print(gtf_df['feature'].unique())
+        # print(gtf_df.dtypes)
+        # print(len(gtf_df.index))
+        
+        # gtf_df = gtf_df.rename(columns={'start': 'cds_start', 'end': 'cds_stop', 'seqname': 'cds_chrom'})
+        
+        # gtf_df.to_csv(f'{self.output_dir}/gtf_v95.tsv', sep='\t', index=False)
+
+        # na_values = 'NA', 'NULL'
+        gtf_df = pd.read_csv(f'{self.output_dir}/gtf_v95.tsv', sep='\t')
+
+        print('Completed')
+
+        return gtf_df
 
     def create_fastas(self):
         fasta_basename = os.path.basename(self.fasta_path)
@@ -68,12 +102,12 @@ class JunctionPipeline():
         return file_name
 
     # creates filtered file
-    # self.ensembl_version
     def filter_regtools_results(self):
         print('Filtering regtools results')
         filter_params = {
             'input_file'  : self.input_file,
             'output_file' : self.create_file_path('filtered'),
+            'gtf_df'      : self.gtf_data,
             'score'       : self.junction_score,
             'distance'    : self.variant_distance,
         }
@@ -91,12 +125,9 @@ class JunctionPipeline():
         if self.normal_sample_name:
             convert_params['normal_sample_name'] = self.normal_sample_name
         print('Converting .vcf to TSV')
-        if os.path.exists(self.create_file_path('annotated')):
-            print('Annotated variant file already exists. Skipping.')
-        else:
-            converter = PvacspliceVcfConverter(**convert_params)
-            converter.execute()
-            print('Completed')
+        converter = PvacspliceVcfConverter(**convert_params)
+        converter.execute()
+        print('Completed')
     
     # creates combined file
     def combine_inputs(self):
@@ -104,8 +135,8 @@ class JunctionPipeline():
             'junctions_df'   : self.filter_df,
             'variant_file'   : self.create_file_path('annotated'),
             'sample_name'    : self.sample_name,
-            #'output_file'    : self.create_file_path('combined'),
-            'maximum_transcript_support_level' : self.maximum_transcript_support_level,
+            'output_file'    : self.create_file_path('combined'),
+            
         }
         print('Combining junction and variant information')
         combined = CombineInputs(**combine_params)
@@ -116,14 +147,15 @@ class JunctionPipeline():
     def junction_to_fasta(self):
         self.final_combined_df = self.combined_df.copy()
         print('Assembling tumor-specific splicing junctions')   
-        for i in self.combined_df.index.unique().to_list():
+        for i in self.combined_df.index.to_list():
             print(i)
             junction = self.combined_df.loc[[i], :]         
             for row in junction.itertuples():
                 junction_params = {
                     'fasta'          : self.personalized_fasta,
                     'junction_df'    : self.combined_df,
-                    'tscript_id'     : row.transcript_name,
+                    'gtf_df'         : self.gtf_data,
+                    'tscript_id'     : row.transcript_id,
                     'chrom'          : row.junction_chrom,
                     'junction_name'  : row.name,
                     'junction_coors' : [row.junction_start, row.junction_stop],
@@ -131,7 +163,7 @@ class JunctionPipeline():
                     'variant_info'   : row.variant_info,
                     'anchor'         : row.anchor,
                     'strand'         : row.strand,
-                    'gene_name'      : row.Gene_name,
+                    'gene_name'      : row.gene_name,
                     'output_file'    : self.create_file_path('fasta'),
                     'output_dir'     : self.output_dir,
                     'sample_name'    : self.sample_name,
