8a16cf6433c68d0bffd400db844bb26bc9201a6d
Susanna Kiwala <susanna.kiwala@wustl.edu>
2023-01-03 12:17:03 -0600

    Fix bug with allele-specific anchors and allow parameters to be set in `pvacseq generate_aggregated_report`

--- pvactools/tools/pvacseq/generate_aggregated_report.py
+++ pvactools/tools/pvacseq/generate_aggregated_report.py
@@ -74,6 +74,25 @@ def define_parser():
         default=1,
         choices=[1,2,3,4,5]
     )
+    parser.add_argument(
+        "--allele-specific-anchors",
+        help="Use allele-specific anchor positions when tiering epitopes in the aggregate report. This option "
+             + "is available for 8, 9, 10, and 11mers and only for HLA-A, B, and C alleles. If this option is "
+             + "not enabled or as a fallback for unsupported lengths and alleles, the default positions of 1, "
+             + "2, epitope length - 1, and epitope length are used. Please see https://doi.org/10.1101/2020.12.08.416271 "
+             + "for more details.",
+        default=False,
+        action='store_true',
+    )
+    parser.add_argument(
+        "--anchor-contribution-threshold", type=float,
+        help="For determining allele-specific anchors, each position is assigned a score based on how binding is "
+             + "influenced by mutations. From these scores, the relative contribution of each position to the "
+             + "overall binding is calculated. Starting with the highest relative contribution, positions whose "
+             + "score together account for the selected contribution threshold are assigned as anchor locations. "
+             + " As a result, a higher threshold leads to the inclusion of more positions to be considered anchors.",
+        default=0.8
+    )
 
     return parser
 
@@ -102,6 +121,8 @@ def main(args_input = sys.argv[1:]):
         expn_val=args.expn_val,
         maximum_transcript_support_level=args.maximum_transcript_support_level,
         top_score_metric=args.top_score_metric,
+        allele_specific_anchors=args.allele_specific_anchors,
+        anchor_contribution_threshold=args.anchor_contribution_threshold,
     ).execute()
     print("Completed")
 
