781f4ae277a948e2c6d51c45476d39a44fc809b8
Megan Richters <meganrichters4@gmail.com>
2023-01-26 14:31:47 -0600

    modify test scripts to match current test_data

--- tests/test_pvacsplice_combine_inputs.py
+++ tests/test_pvacsplice_combine_inputs.py
@@ -2,8 +2,8 @@ import unittest
 import os
 import sys
 import tempfile
-from subprocess import call
 from filecmp import cmp
+import pandas as pd
 import py_compile
 
 from pvactools.lib.combine_inputs import CombineInputs
@@ -14,37 +14,29 @@ class CombineInputsTests(unittest.TestCase):
     def setUpClass(cls):
         cls.python        = sys.executable
         cls.executable    = os.path.join(pvactools_directory(), "pvactools", "tools", "pvacsplice", "combine_inputs.py")
-        cls.test_data_dir = os.path.join(pvactools_directory(), "tests", "test_data", "pvacsplice", "combine_inputs")
+        cls.test_data_dir = os.path.join(pvactools_directory(), "tests", "test_data", "pvacsplice", 'results')
+        # inputs 
+        cls.junctions_df = pd.read_csv(os.path.join(cls.test_data_dir, 'Test.10_100_filtered.tsv'), sep='\t') # default filters
+        cls.variant_file = os.path.join(cls.test_data_dir, 'Test.annotated.tsv')
 
     def module_compiles(self):
         self.assertTrue(py_compile.compile(self.executable))
 
     def test_combine_inputs_runs_and_produces_expected_output(self):
-        junctions_file = os.path.join(self.test_data_dir, 'Test_filtered.tsv')
-        variant_file = os.path.join(self.test_data_dir, 'Test_annotated.tsv')
         output_dir = tempfile.TemporaryDirectory()
-
-        # iterate free params
-        for tsl in [1, 3, 5]:
-            output_file   = os.path.join(output_dir.name, 'sample_{}_combined.tsv'.format(tsl))
-            #output_file   = os.path.join(self.test_data_dir, 'Test_{}_combined.tsv'.format(tsl))
-            params = {
-                'junctions_file'                    : junctions_file,
-                'variant_file'                      : variant_file,
-                #'sample_name'                       : 'sample_{}'.format(tsl),
-                'output_file'                       : output_file,
-                'maximum_transcript_support_level'  : tsl,
-            }
-            combined = CombineInputs(**params)
-            combined.execute()
-
-            #direct file comparison
-            for file_name in (
-                'sample_{}_combined.tsv'.format(tsl),
-                #'sample_{}_annotated_filtered.tsv'.format(tsl),
-            ):
-                output_file   = os.path.join(output_dir.name, file_name)
-                expected_file = os.path.join(self.test_data_dir, file_name.replace('sample', 'Test'))
-                self.assertTrue(compare(output_file, expected_file), "files don't match {} - {}".format(output_file, expected_file))
+        output_file = os.path.join(output_dir.name, 'sample_combined.tsv')
+        params = {
+            'junctions_df' : self.junctions_df,
+            'variant_file' : self.variant_file,
+            'output_file'  : output_file,
+            'output_dir'   : output_dir
+        }
+        CombineInputs(**params).execute()
+        expected_file = os.path.join(self.test_data_dir, 'Test.combined.tsv')
+        self.assertTrue(cmp(
+            output_file, 
+            expected_file), 
+            "files don't match {} - {}".format(output_file, expected_file)
+            )
 
         output_dir.cleanup()
