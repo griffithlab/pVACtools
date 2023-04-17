f1639a9df9495a356d62b22ae96575e7ea8dbcb0
Susanna Kiwala <susanna.kiwala@wustl.edu>
2023-04-17 14:50:09 -0500

    Fix blast status test

--- api_status_tests/test_blast.py
+++ api_status_tests/test_blast.py
@@ -5,10 +5,10 @@ from tests.utils import *
 
 
 class BlastApiStatusTest(unittest.TestCase):
-    def test_netchop_api_runs_and_returns_expected_value(self):
+    def test_blast_api_runs_and_returns_expected_value(self):
         result_handle = NCBIWWW.qblast("blastp", "refseq_select_prot", "AAIMYVPALGWEFLAFTRLTSELNFLLQEID", entrez_query="Homo Sapiens [Organism]", word_size=7, gapcosts='32767 32767')
         expected_file_name = os.path.join(pvactools_directory(), 'api_status_tests', 'blast_response.xml')
         with open(expected_file_name, 'r') as expected_fh:
-            actual_content = response.read()
+            actual_content = result_handle.read()
             expected_content = expected_fh.read()
             self.assertEqual(expected_content, actual_content)
