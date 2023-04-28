80ecea4313bfb6b995ed0cca6b164125d846bf79
Susanna Kiwala <susanna.kiwala@wustl.edu>
2023-04-28 09:55:44 -0500

    Update top score filter to mirror how the aggregate report determines the Best Peptide

--- pvactools/tools/pvacseq/top_score_filter.py
+++ pvactools/tools/pvacseq/top_score_filter.py
@@ -9,7 +9,16 @@ def main(args_input = sys.argv[1:]):
     parser = define_parser()
     args = parser.parse_args(args_input)
 
-    TopScoreFilter(args.input_file, args.output_file, args.top_score_metric, 'pVACseq').execute()
+    TopScoreFilter(
+        args.input_file,
+        args.output_file,
+        args.top_score_metric,
+        args.binding_threshold,
+        args.allele_specific_binding_thresholds,
+        args.allele_specific_anchors,
+        args.anchor_contribution_threshold,
+        'pVACseq'
+    ).execute()
 
 if __name__ == "__main__":
     main()
