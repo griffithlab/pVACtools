78ccdb0650da4446c49a44486ee50c62d82baf5d
Susanna Kiwala <susanna.kiwala@wustl.edu>
2023-05-31 13:50:07 -0500

    Handle cases during reference proteome match where a FS wt peptide is fully in the mt peptide

--- tests/test_calculate_reference_proteome_similarity.py
+++ tests/test_calculate_reference_proteome_similarity.py
@@ -39,6 +39,24 @@ class CalculateReferenceProteomeSimilarityTests(unittest.TestCase):
             os.remove(metric_file)
             close_mock_fhs()
 
+    def test_wt_peptide_fully_in_mt_peptide(self):
+        with unittest.mock.patch('Bio.Blast.NCBIWWW.qblast', side_effect=mock_ncbiwww_qblast):
+            input_file = os.path.join(self.test_data_dir, 'input_wt_in_mt.tsv')
+            input_fasta = os.path.join(self.test_data_dir, 'input_wt_in_mt.fasta')
+            output_file = tempfile.NamedTemporaryFile()
+            metric_file = "{}.reference_matches".format(output_file.name)
+            self.assertFalse(CalculateReferenceProteomeSimilarity(input_file, input_fasta, output_file.name).execute())
+            self.assertTrue(cmp(
+                output_file.name,
+                os.path.join(self.test_data_dir, "output_wt_in_mt.tsv"),
+            ))
+            self.assertTrue(cmp(
+                metric_file,
+                os.path.join(self.test_data_dir, "output_wt_in_mt.tsv.reference_matches"),
+            ))
+            os.remove(metric_file)
+            close_mock_fhs()
+
     def test_blastp_db_incompatible_with_species(self):
         with self.assertRaises(Exception) as context:
             input_file = os.path.join(self.test_data_dir, 'input.tsv')
