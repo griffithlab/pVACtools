dcaf10624d2df9b92969596eff1c4173172784f9
Susanna Kiwala <susanna.kiwala@wustl.edu>
2024-05-10 10:34:54 -0500

    Add standalone helper commands

--- pvactools/tools/pvacsplice/__init__.py
+++ pvactools/tools/pvacsplice/__init__.py
@@ -1,9 +1,17 @@
 __all__ = [
+    'allele_specific_cutoffs',
     'binding_filter',
+    'calculate_reference_proteome_similarity',
     'coverage_filter',
+    'generate_aggregated_report',
+    'identify_problematic_amino_acids',
+    'install_vep_plugin',
+    'net_chop',
+    'netmhc_stab',
     'run',
     'top_score_filter',
     'transcript_support_level_filter',
+    'valid_alleles',
 ]
 
 from . import *
--- pvactools/tools/pvacsplice/main.py
+++ pvactools/tools/pvacsplice/main.py
@@ -44,6 +44,62 @@ def define_parser():
     )
     top_score_filter_parser.set_defaults(func=top_score_filter)
 
+    net_chop_parser = subparsers.add_parser(
+        "net_chop",
+        help="Run NetChop on existing pVACsplice output .tsv to predict cleavage sites on the neoepitopes.",
+        add_help=False,
+    )
+    net_chop_parser.set_defaults(func=net_chop)
+
+    netmhc_stab_parser = subparsers.add_parser(
+        "netmhc_stab",
+        help="Run NetMHCStabPan on existing pVACsplice output .tsv to add stability predictions to the neoepitopes.",
+        add_help=False,
+    )
+    netmhc_stab_parser.set_defaults(func=netmhc_stab)
+
+    calculate_reference_proteome_similarity_parser = subparsers.add_parser(
+        "calculate_reference_proteome_similarity",
+        help="Blast peptides against the reference proteome on existing pVACsplice output .tsv.",
+        add_help=False
+    )
+    calculate_reference_proteome_similarity_parser.set_defaults(func=calculate_reference_proteome_similarity)
+
+    generate_aggregated_report_parser = subparsers.add_parser(
+        "generate_aggregated_report",
+        help="Generate an aggregated report from a pVACsplice .all_epitopes.tsv report file.",
+        add_help=False
+    )
+    generate_aggregated_report_parser.set_defaults(func=generate_aggregated_report)
+
+    identify_problematic_amino_acids_parser = subparsers.add_parser(
+        "identify_problematic_amino_acids",
+        help="Mark problematic amino acid positions in each epitope or filter entries that have problematic amino acids.",
+        add_help = False
+        )
+    identify_problematic_amino_acids_parser.set_defaults(func=identify_problematic_amino_acids)
+
+    install_vep_plugin_parser = subparsers.add_parser(
+        "install_vep_plugin",
+        help="Install the Wildtype and Frameshift VEP plugins into your VEP_plugins directory.",
+        add_help=False
+    )
+    install_vep_plugin_parser.set_defaults(func=install_vep_plugin)
+
+    valid_alleles_parser = subparsers.add_parser(
+        "valid_alleles",
+        help="Show a list of valid allele names.",
+        add_help=False
+    )
+    valid_alleles_parser.set_defaults(func=valid_alleles)
+
+    allele_specific_cutoffs_parser = subparsers.add_parser(
+        "allele_specific_cutoffs",
+        help="Show the allele specific cutoffs.",
+        add_help=False,
+    )
+    allele_specific_cutoffs_parser.set_defaults(func=allele_specific_cutoffs)
+
     gtf_to_tsv = subparsers.add_parser(
         "gtf_to_tsv",
         help="Filter and convert reference GTF to TSV format.",
