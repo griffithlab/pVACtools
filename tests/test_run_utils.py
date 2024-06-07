47798c48f9a7cb29605d0f743721ca89123188b1
ldhtnp <ldhtnp@umsystem.edu>
2024-06-07 09:48:29 -0500

    added tests for new anchor position functionality

--- tests/test_run_utils.py
+++ tests/test_run_utils.py
@@ -5,6 +5,7 @@ from filecmp import cmp
 import sys
 import py_compile
 
+from pvactools.lib.aggregate_all_epitopes import PvacseqAggregateAllEpitopes
 from pvactools.lib.run_utils import *
 from tests.utils import *
 
@@ -14,6 +15,7 @@ class RunUtilsTests(unittest.TestCase):
     def setUpClass(cls):
         #locate the bin
         cls.utils_path = os.path.join(pvactools_directory(), "pvactools", "lib", "run_utils.py")
+        cls.test_data_dir = os.path.join(pvactools_directory(), "tests", "test_data", "run_utils")
 
     def module_compiles(self):
         self.assertTrue(py_compile.compile(self.utils_path))
@@ -34,4 +36,22 @@ class RunUtilsTests(unittest.TestCase):
         self.assertEqual(
             sorted(combine_class_ii_alleles(["DRB9*01:02", "DRA*01:01"])),
             sorted(["DRB9*01:02", "DRA*01:01"])
+        )
+    
+    def test_get_anchor_positions(self):
+        class_object = PvacseqAggregateAllEpitopes(input_file=os.path.join(self.test_data_dir, 'M_GC-OxParp_A-OxParp_A_FF_DNA.all_epitopes.tsv'), 
+                                                   output_file="",
+                                                   allele_specific_anchors=True
+                                                  )
+
+        # anchor positions are stored for the given valid mouse allele and epitope length
+        self.assertEqual(
+            get_anchor_positions(class_object, "H-2-Db", 11),
+            [5, 11]
+        )
+
+        # valid mouse allele, but positions not stored for given epitope length
+        self.assertEqual(
+            get_anchor_positions(class_object, "H-2-Kb", 11),
+            [1, 2, 10, 11]
         )
