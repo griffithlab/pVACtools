ff36812488e43db1935db6117a2da0f050e19918
Susanna Kiwala <susanna.kiwala@wustl.edu>
2025-05-27 20:15:14 -0500

    Pull coverage_filter tests to own test files

--- tests/test_pvacsplice.py
+++ tests/test_pvacsplice.py
@@ -68,7 +68,6 @@ class PvacspliceTests(unittest.TestCase):
             )
         usage_search = re.compile(r"usage: ")
         for command in [
-            "coverage_filter",
             "run",
             "top_score_filter",
             "transcript_support_level_filter",
@@ -99,21 +98,6 @@ class PvacspliceTests(unittest.TestCase):
             "run.py"
         ))
         self.assertTrue(compiled_run_path)
-
-    def test_coverage_filter_compiles(self):
-        compiled_run_path = py_compile.compile(os.path.join(
-            self.pvactools_directory,
-            'pvactools',
-            "tools",
-            "pvacsplice",
-            "coverage_filter.py"
-        ))
-        self.assertTrue(compiled_run_path)
-
-    def test_coverage_filter_runs(self):
-        input_file = os.path.join(self.test_data_directory, 'results', 'Test.all_epitopes.tsv')
-        output_file = tempfile.NamedTemporaryFile()
-        coverage_filter.main([input_file, output_file.name])
 
     def test_transcript_support_level_filter_compiles(self):
         compiled_run_path = py_compile.compile(os.path.join(
