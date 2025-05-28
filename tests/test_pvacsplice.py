434a714e81151d3776aaaa7859d66e7578098017
Susanna Kiwala <susanna.kiwala@wustl.edu>
2025-05-28 08:24:24 -0500

    Pull generate_aggregated_report tests to own test files

--- tests/test_pvacsplice.py
+++ tests/test_pvacsplice.py
@@ -71,7 +71,6 @@ class PvacspliceTests(unittest.TestCase):
             "run",
             "top_score_filter",
             "transcript_support_level_filter",
-            "generate_aggregated_report",
             "generate_protein_fasta",
             "net_chop",
             "netmhc_stab",
@@ -127,21 +126,6 @@ class PvacspliceTests(unittest.TestCase):
         input_file = os.path.join(self.test_data_directory, 'results', 'Test.all_epitopes.tsv')
         output_file = tempfile.NamedTemporaryFile()
         top_score_filter.main([input_file, output_file.name])
-
-    def test_generate_aggregated_report_compiles(self):
-        compiled_run_path = py_compile.compile(os.path.join(
-            self.pvactools_directory,
-            'pvactools',
-            "tools",
-            "pvacsplice",
-            "generate_aggregated_report.py"
-        ))
-        self.assertTrue(compiled_run_path)
-
-    def test_generate_aggregated_report_runs(self):
-        input_file = os.path.join(self.test_data_directory, 'results', 'Test.all_epitopes.tsv')
-        output_file = tempfile.NamedTemporaryFile()
-        generate_aggregated_report.main([input_file, output_file.name])
 
     def test_net_chop_compiles(self):
         compiled_run_path = py_compile.compile(os.path.join(
