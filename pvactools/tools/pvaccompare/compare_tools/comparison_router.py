d32122178e143ceeb841ae8e2c230af0ffb71329
ldhtnp <ldhtnp@umsystem.edu>
2025-06-10 10:18:21 -0500

    fixed file paths

--- pvactools/tools/pvaccompare/compare_tools/comparison_router.py
+++ pvactools/tools/pvaccompare/compare_tools/comparison_router.py
@@ -16,18 +16,23 @@ def find_file(results_folder, subfolder, pattern):
 
 
 def get_prefix(class_type, results_folder):
-    if os.path.exists(os.path.join(results_folder, "MHC_Class_I")) or os.path.exists(
-        os.path.join(results_folder, "MHC_Class_II")
-    ):
-        return "MHC_Class_I" if class_type == "1" else "MHC_Class_II"
-    elif os.path.exists(
-        os.path.join(results_folder, "pVACseq/mhc_i")
-    ) or os.path.exists(os.path.join(results_folder, "pVACseq/mhc_ii")):
-        return "pVACseq/mhc_i" if class_type == "1" else "pVACseq/mhc_ii"
-    else:
-        raise FileNotFoundError(
-            f"Could not locate result files for folder: {results_folder}"
-        )
+    if class_type == "1":
+        if os.path.exists(os.path.join(results_folder, "MHC_Class_I")):
+            return "MHC_Class_I"
+    elif class_type == "2":
+        if os.path.exists(os.path.join(results_folder, "MHC_Class_II")):
+            return "MHC_Class_II"
+
+    if class_type == "1":
+        if os.path.exists(os.path.join(results_folder, "pVACseq/mhc_i")):
+            return "pVACseq/mhc_i"
+    elif class_type == "2":
+        if os.path.exists(os.path.join(results_folder, "pVACseq/mhc_ii")):
+            return "pVACseq/mhc_ii"
+
+    raise FileNotFoundError(
+        f"Could not locate result files for folder: {results_folder} and class {class_type}"
+    )
 
 
 def run_comparison(
@@ -46,68 +51,65 @@ def run_comparison(
     """
     folder1_prefix = get_prefix(class_type, results_folder1)
     folder2_prefix = get_prefix(class_type, results_folder2)
-    output_path = (
-        f'{output_dir}/{"mhc_class_i" if class_type == "1" else "mhc_class_ii"}'
-    )
+    output_path = os.path.join(output_dir, "mhc_class_i" if class_type == "1" else "mhc_class_ii")
 
-    yml1_path = find_file(results_folder1, folder1_prefix + "/log", "inputs.yml")
-    yml2_path = find_file(results_folder2, folder2_prefix + "/log", "inputs.yml")
+    yml1_path = find_file(results_folder1, os.path.join(folder1_prefix, "log"), "inputs.yml")
+    yml2_path = find_file(results_folder2, os.path.join(folder2_prefix, "log"), "inputs.yml")
     if yml1_path and yml2_path:
         logging.info("Running the input YML comparison tool...")
         run_compare_yml(yml1_path, yml2_path, output_path, class_type)
         logging.info("\u2713 Comparison completed successfully.")
     else:
-        if yml1_path:
-            logging.error(
-                "ERROR: Could not locate the input YML file in results folder 2 for MHC Class %s.",
-                "I" if class_type == "1" else "II",
-            )
-        elif yml2_path:
-            logging.error(
-                "ERROR: Could not locate the input YML file in results folder 1 for MHC Class %s.",
-                "I" if class_type == "1" else "II",
-            )
-        else:
+        if not yml1_path and not yml2_path:
             logging.error(
                 "ERROR: Could not locate the input YML file in either results folder for MHC Class %s.",
                 "I" if class_type == "1" else "II",
             )
-
+        elif not yml1_path:
+            logging.error(
+                "ERROR: Could not locate the input YML file in results folder 1 for MHC Class %s.",
+                "I" if class_type == "1" else "II",
+            )
+        elif not yml2_path:
+            logging.error(
+                "ERROR: Could not locate the input YML file in results folder 2 for MHC Class %s.",
+                "I" if class_type == "1" else "II",
+            )
         logging.info("\u2716 Comparison skipped.")
 
     json1_path = find_file(
-        results_folder1, folder1_prefix + "/", "*all_epitopes.aggregated.metrics.json"
+        results_folder1, folder1_prefix, "*all_epitopes.aggregated.metrics.json"
     )
     json2_path = find_file(
-        results_folder2, folder2_prefix + "/", "*all_epitopes.aggregated.metrics.json"
+        results_folder2, folder2_prefix, "*all_epitopes.aggregated.metrics.json"
     )
     if json1_path and json2_path:
         logging.info("\nRunning the metrics JSON comparison tool...")
         run_compare_json(json1_path, json2_path, output_path, class_type)
         logging.info("\u2713 Comparison completed successfully.")
     else:
-        if json1_path:
+        if not json1_path and not json2_path:
             logging.error(
-                "ERROR: Could not locate the metrics JSON file in results folder 2 for MHC Class %s.",
+                "ERROR: Could not locate the metrics JSON file in either results folder for MHC Class %s.",
                 "I" if class_type == "1" else "II",
             )
-        elif json2_path:
+        elif not json1_path:
             logging.error(
                 "ERROR: Could not locate the metrics JSON file in results folder 1 for MHC Class %s.",
                 "I" if class_type == "1" else "II",
             )
-        else:
+        elif not json2_path:
             logging.error(
-                "ERROR: Could not locate the metrics JSON file in either results folder for MHC Class %s.",
+                "ERROR: Could not locate the metrics JSON file in results folder 2 for MHC Class %s.",
                 "I" if class_type == "1" else "II",
             )
         logging.info("\u2716 Comparison skipped.")
 
     agg_tsv1_path = find_file(
-        results_folder1, folder1_prefix + "/", "*all_epitopes.aggregated.tsv"
+        results_folder1, folder1_prefix, "*all_epitopes.aggregated.tsv"
     )
     agg_tsv2_path = find_file(
-        results_folder2, folder2_prefix + "/", "*all_epitopes.aggregated.tsv"
+        results_folder2, folder2_prefix, "*all_epitopes.aggregated.tsv"
     )
     if agg_tsv1_path and agg_tsv2_path:
         logging.info("\nRunning the aggregated TSV comparison tool...")
@@ -116,28 +118,28 @@ def run_comparison(
         )
         logging.info("\u2713 Comparison completed successfully.")
     else:
-        if agg_tsv1_path:
+        if not agg_tsv1_path and not agg_tsv2_path:
             logging.error(
-                "ERROR: Could not locate the aggregated TSV file in results folder 2 for MHC Class %s.",
+                "ERROR: Could not locate the aggregated TSV file in either results folder for MHC Class %s.",
                 "I" if class_type == "1" else "II",
             )
-        elif agg_tsv2_path:
+        elif not agg_tsv1_path:
             logging.error(
                 "ERROR: Could not locate the aggregated TSV file in results folder 1 for MHC Class %s.",
                 "I" if class_type == "1" else "II",
             )
-        else:
+        elif not agg_tsv2_path:
             logging.error(
-                "ERROR: Could not locate the aggregated TSV file in either results folder for MHC Class %s.",
+                "ERROR: Could not locate the aggregated TSV file in results folder 2 for MHC Class %s.",
                 "I" if class_type == "1" else "II",
             )
         logging.info("\u2716 Comparison skipped.")
 
     unagg_tsv1_path = find_file(
-        results_folder1, folder1_prefix + "/", "*all_epitopes.tsv"
+        results_folder1, folder1_prefix, "*all_epitopes.tsv"
     )
     unagg_tsv2_path = find_file(
-        results_folder2, folder2_prefix + "/", "*all_epitopes.tsv"
+        results_folder2, folder2_prefix, "*all_epitopes.tsv"
     )
     if unagg_tsv1_path and unagg_tsv2_path:
         logging.info("\nRunning the unaggregated TSV comparison tool...")
@@ -150,28 +152,28 @@ def run_comparison(
         )
         logging.info("\u2713 Comparison completed successfully.")
     else:
-        if unagg_tsv1_path:
+        if not unagg_tsv1_path and not unagg_tsv2_path:
             logging.error(
-                "ERROR: Could not locate the unaggregated TSV file in results folder 2 for MHC Class %s.",
+                "ERROR: Could not locate the unaggregated TSV file in either results folder for MHC Class %s.",
                 "I" if class_type == "1" else "II",
             )
-        elif unagg_tsv2_path:
+        elif not unagg_tsv1_path:
             logging.error(
                 "ERROR: Could not locate the unaggregated TSV file in results folder 1 for MHC Class %s.",
                 "I" if class_type == "1" else "II",
             )
-        else:
+        elif not unagg_tsv2_path:
             logging.error(
-                "ERROR: Could not locate the unaggregated TSV file in either results folder for MHC Class %s.",
+                "ERROR: Could not locate the unaggregated TSV file in results folder 2 for MHC Class %s.",
                 "I" if class_type == "1" else "II",
             )
         logging.info("\u2716 Comparison skipped.")
 
     refmatch_tsv1_path = find_file(
-        results_folder1, folder1_prefix + "/", "*.reference_matches"
+        results_folder1, folder1_prefix, "*.reference_matches"
     )
     refmatch_tsv2_path = find_file(
-        results_folder2, folder2_prefix + "/", "*.reference_matches"
+        results_folder2, folder2_prefix, "*.reference_matches"
     )
     if refmatch_tsv1_path and refmatch_tsv2_path:
         logging.info("\nRunning the reference match TSV comparison tool...")
@@ -184,19 +186,19 @@ def run_comparison(
         )
         logging.info("\u2713 Comparison completed successfully.")
     else:
-        if refmatch_tsv1_path:
+        if not refmatch_tsv1_path and not refmatch_tsv2_path:
             logging.error(
-                "ERROR: Could not locate the reference match TSV file in results folder 2 for MHC Class %s.",
+                "ERROR: Could not locate the reference match TSV file in either results folder for MHC Class %s.",
                 "I" if class_type == "1" else "II",
             )
-        elif refmatch_tsv2_path:
+        elif not refmatch_tsv1_path:
             logging.error(
                 "ERROR: Could not locate the reference match TSV file in results folder 1 for MHC Class %s.",
                 "I" if class_type == "1" else "II",
             )
-        else:
+        elif not refmatch_tsv2_path:
             logging.error(
-                "ERROR: Could not locate the reference match TSV file in either results folder for MHC Class %s.",
+                "ERROR: Could not locate the reference match TSV file in results folder 2 for MHC Class %s.",
                 "I" if class_type == "1" else "II",
             )
         logging.info("\u2716 Comparison skipped.")
