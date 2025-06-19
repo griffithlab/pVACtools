49c7662300b2024705a0a3dd4bc8f9690064dd7c
jaz2021 <jazeb2021@gmail.com>
2025-06-19 12:22:50 -0500

    Fixed problem where when filename_prefix is not specified program fails

--- pvactools/lib/pipeline.py
+++ pvactools/lib/pipeline.py
@@ -421,8 +421,9 @@ class Pipeline(metaclass=ABCMeta):
 
     def combined_parsed_path(self):
         combined_parsed = "%s.all_epitopes.tsv" % self.sample_name
-        if self.filename_prefix != None:
-            combined_parsed = "{}.{}".format(self.filename_prefix, combined_parsed)
+        if hasattr(self, "filename_prefix"):
+            if self.filename_prefix != None:
+                combined_parsed = "{}.{}".format(self.filename_prefix, combined_parsed)
         return os.path.join(self.output_dir, combined_parsed)
 
     def combined_parsed_outputs(self, split_parsed_output_files):
@@ -441,8 +442,9 @@ class Pipeline(metaclass=ABCMeta):
 
     def final_path(self):
         output_str = self.sample_name + ".filtered.tsv"
-        if self.filename_prefix != None:
-            output_str = "{}.{}".format(self.filename_prefix, output_str)
+        if hasattr(self, "filename_prefix"):
+            if self.filename_prefix != None:
+                output_str = "{}.{}".format(self.filename_prefix, output_str)
         return os.path.join(self.output_dir, output_str)
 
     def execute(self):
