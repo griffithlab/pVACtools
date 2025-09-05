666673022a20b6a522aef5e34e21c4a54c9921a7
ldhtnp <ldhtnp@umsystem.edu>
2025-09-05 09:54:24 -0500

    implemented requested changes

--- pvactools/lib/generate_reviews_files.py
+++ pvactools/lib/generate_reviews_files.py
@@ -93,7 +93,7 @@ def fill_variant_called_column(df, input_vcf, external_vcf):
 
 
 def main(peptides_path, classI_path, classII_path, input_vcf, external_vcf, sample_name,
-        all_epitopes_flag, allowed_evaluations, output_file_prefix, output_path):
+        allowed_evaluations, output_file_prefix, output_path):
     reviewed_candidates = pd.read_csv(classI_path, sep="\t")
     reviewed_candidates = reviewed_candidates[reviewed_candidates["Evaluation"].isin(allowed_evaluations)]
 
@@ -176,53 +176,39 @@ def main(peptides_path, classI_path, classII_path, input_vcf, external_vcf, samp
 
     classI = pd.read_csv(classI_path, sep="\t")
     classII = pd.read_csv(classII_path, sep="\t")
-    if all_epitopes_flag:
-        classI.rename(columns = {"MT Epitope Seq":"Best Peptide Class I", "HLA Allele":"Class I Allele", 
-                                 "Median MT IC50 Score":"Class I IC50 MT", "Median MT Percentile":"Class I %ile MT", 
-                                 "Transcript":"Class I Best Transcript"}, inplace=True)
-        classII.rename(columns = {"MT Epitope Seq":"Best Peptide Class II", "HLA Allele":"Class II Allele", 
-                                  "Median MT IC50 Score":"Class II IC50 MT", "Median MT Percentile":"Class II %ile MT", 
-                                  "Transcript":"Class II Best Transcript"}, inplace=True)
-        
-        classI['position AA Change'] = classI['Index'].apply(lambda x: x.split('.')[5])
-        classI['51mer ID'] = classI['Gene Name'] + '.' + classI['Class I Best Transcript'] + '.' + classI['position AA Change'] 
-        class_sequences = pd.merge(classI[['Index', 'Best Peptide Class I', '51mer ID', 'Pos', 'AA Change', 'Class I Allele', "Class I IC50 MT", "Class I %ile MT", "Class I Best Transcript"]], 
-                                   classII[['Index', 'Best Peptide Class II', 'Class II Allele', "Class II IC50 MT", "Class II %ile MT", "Class II Best Transcript"]], on='ID', how='left')
-        class_sequences = class_sequences.drop(columns=['Index'])
 
-    else:
-        classI.rename(columns = {"Best Peptide":"Best Peptide Class I", "Allele":"Class I Allele", 
-                                 "IC50 MT":"Class I IC50 MT", "%ile MT":"Class I %ile MT", 
-                                 "Best Transcript":"Class I Best Transcript"}, inplace=True)
-        classII.rename(columns = {"Best Peptide":"Best Peptide Class II", "Allele":"Class II Allele", 
-                                  "IC50 MT":"Class II IC50 MT", "%ile MT":"Class II %ile MT", 
-                                  "Best Transcript":"Class II Best Transcript"}, inplace=True)
+    classI.rename(columns = {"Best Peptide":"Best Peptide Class I", "Allele":"Class I Allele", 
+                                "IC50 MT":"Class I IC50 MT", "%ile MT":"Class I %ile MT", 
+                                "Best Transcript":"Class I Best Transcript"}, inplace=True)
+    classII.rename(columns = {"Best Peptide":"Best Peptide Class II", "Allele":"Class II Allele", 
+                                "IC50 MT":"Class II IC50 MT", "%ile MT":"Class II %ile MT", 
+                                "Best Transcript":"Class II Best Transcript"}, inplace=True)
+
+    def rearrange_string(s):
+        # Handle insertions like -287-288L → 287-288-/L
+        insertion_match = re.match(r'-?(\d+-\d+)([A-Za-z]+)', s)
+        if insertion_match:
+            position = insertion_match.group(1)
+            inserted = insertion_match.group(2)
+            return f"{position}-/{inserted}"
+
+        # Handle substitutions like E510Q → 510E/Q
+        substitution_match = re.match(r'([A-Za-z]+)(\d+)([A-Za-z]+)', s)
+        if substitution_match:
+            letters_before = substitution_match.group(1)
+            numbers = substitution_match.group(2)
+            letters_after = substitution_match.group(3)
+            return f"{numbers}{letters_before}/{letters_after}"
+
+        # Return unchanged if no pattern matched
+        return s
     
-        def rearrange_string(s):
-            # Handle insertions like -287-288L → 287-288-/L
-            insertion_match = re.match(r'-?(\d+-\d+)([A-Za-z]+)', s)
-            if insertion_match:
-                position = insertion_match.group(1)
-                inserted = insertion_match.group(2)
-                return f"{position}-/{inserted}"
 
-            # Handle substitutions like E510Q → 510E/Q
-            substitution_match = re.match(r'([A-Za-z]+)(\d+)([A-Za-z]+)', s)
-            if substitution_match:
-                letters_before = substitution_match.group(1)
-                numbers = substitution_match.group(2)
-                letters_after = substitution_match.group(3)
-                return f"{numbers}{letters_before}/{letters_after}"
-
-            # Return unchanged if no pattern matched
-            return s
-        
-    
-        classI['position AA Change'] = classI['AA Change'].apply(rearrange_string)
-        classI['51mer ID'] = classI['Gene'] + '.' + classI['Class I Best Transcript'] + '.' + classI['position AA Change'] 
-        class_sequences = pd.merge(classI[['ID', 'Best Peptide Class I', '51mer ID', 'Pos', 'AA Change', 'Class I Allele', "Class I IC50 MT", "Class I %ile MT", "Class I Best Transcript"]], 
-                                   classII[['ID', 'Best Peptide Class II', 'Class II Allele', "Class II IC50 MT", "Class II %ile MT", "Class II Best Transcript"]], on='ID', how='left')
-        class_sequences = class_sequences.drop(columns=['ID'])
+    classI['position AA Change'] = classI['AA Change'].apply(rearrange_string)
+    classI['51mer ID'] = classI['Gene'] + '.' + classI['Class I Best Transcript'] + '.' + classI['position AA Change'] 
+    class_sequences = pd.merge(classI[['ID', 'Best Peptide Class I', '51mer ID', 'Pos', 'AA Change', 'Class I Allele', "Class I IC50 MT", "Class I %ile MT", "Class I Best Transcript"]], 
+                                classII[['ID', 'Best Peptide Class II', 'Class II Allele', "Class II IC50 MT", "Class II %ile MT", "Class II Best Transcript"]], on='ID', how='left')
+    class_sequences = class_sequences.drop(columns=['ID'])
 
     merged_peptide_51mer = pd.merge(peptides, class_sequences, on='51mer ID', how='left')
     
