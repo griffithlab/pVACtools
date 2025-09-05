61704058cf4a4283bc30d3fcc645bfd56704897a
ldhtnp <ldhtnp@umsystem.edu>
2025-09-05 07:39:58 -0500

    updated tests

--- tests/test_compare.py
+++ tests/test_compare.py
@@ -122,6 +122,10 @@ class TestRunCompare(unittest.TestCase):
                     file_path = os.path.join(folder, file_name)
                     with open(test_data_path, "r") as test_file, open(file_path, "w") as temp_file:
                         temp_file.write(test_file.read())
+            
+            extra_all_epitopes_file = os.path.join(input2_mhc_class_i, "additional_all_epitopes.tsv")
+            with open(test_data_files["all_epitopes.tsv"], "r") as src, open(extra_all_epitopes_file, "w") as dst:
+                dst.write(src.read())
 
             args_list = [
                 input1,
@@ -137,8 +141,9 @@ class TestRunCompare(unittest.TestCase):
             with self.assertLogs(level="INFO") as log:
                 compare.main(args_list)
 
-            self.assertIn("ERROR:root:ERROR: Could not locate the input YML file in either results folder for MHC Class I.", log.output)
+            self.assertIn("ERROR:root:ERROR: Could not locate the input YML file in results folder 1 for MHC Class I.", log.output)
+            self.assertIn("ERROR:root:ERROR: Could not locate the input YML file in results folder 2 for MHC Class I.", log.output)
             self.assertIn("INFO:root:The JSON metric inputs are identical.", log.output)
-            self.assertIn("INFO:root:The Unaggregated TSV files are identical.", log.output)
+            self.assertIn("ERROR:root:ERROR: Located multiple unaggregated TSV files in results folder 2 for MHC Class I:", log.output)
             self.assertIn("INFO:root:The Aggregated TSV files are identical.", log.output)
             self.assertIn("INFO:root:Successfully generated MHC Class I comparison report.", log.output)
