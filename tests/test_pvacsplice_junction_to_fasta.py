6a58273a00dfac561abf8f399fe6cc055987e939
Susanna Kiwala <susanna.kiwala@wustl.edu>
2026-07-16 11:45:06 -0500

    Speed up junction to fasta tests

--- tests/test_pvacsplice_junction_to_fasta.py
+++ tests/test_pvacsplice_junction_to_fasta.py
@@ -17,18 +17,22 @@ class JunctionToFastaTests(unittest.TestCase):
         cls.python = sys.executable
         cls.executable = os.path.join(pvactools_directory(), "pvactools", "lib", "junction_to_fasta.py")
         cls.test_data_dir = os.path.join(pvactools_directory(), "tests", "test_data", "pvacsplice")
+        cls.fasta_file = gunzip_file(os.path.join(cls.test_data_dir, 'inputs', 'all_sequences_chr1.fa.gz'))
+        cls.combined_df = pd.read_csv(os.path.join(cls.test_data_dir, 'results', 'Test.combined.tsv'), sep='\t', na_values="NA", dtype={'transcript_support_level': str})
+        cls.gtf_df = pd.read_csv(os.path.join(cls.test_data_dir, 'results', 'Test.filtered.gtf.tsv'), sep='\t', na_values="NA", dtype={'transcript_support_level': str})
+        cls.annotated_vcf = os.path.join(pvactools_directory(), cls.test_data_dir, 'inputs', 'annotated.expression_chr1.vcf.gz')
+        cls.sample_name = 'HCC1395_TUMOR_DNA'
+
+    @classmethod
+    def tearDownClass(cls):
+        os.unlink(cls.fasta_file)
 
     def test_module_compiles(self):
         self.assertTrue(py_compile.compile(self.executable))
 
     # using GBM data instead? - how about i run one with current gtf and fa - one of every type going through the pipeline
     def test_junction_to_fasta_runs_and_produces_expected_output(self):
-        combined_df   = pd.read_csv(os.path.join(self.test_data_dir, 'results', 'Test.combined.tsv'), sep='\t', na_values="NA", dtype={'transcript_support_level': str})
-        gtf_df        = pd.read_csv(os.path.join(self.test_data_dir, 'results', 'Test.gtf.tsv'), sep='\t', na_values="NA", dtype={'transcript_support_level': str})
-        annotated_vcf = os.path.join(pvactools_directory(), self.test_data_dir, 'inputs', 'annotated.expression_chr1.vcf.gz')
-        sample_name   = 'HCC1395_TUMOR_DNA'
-        fasta_path = os.path.join(self.test_data_dir, 'inputs', 'all_sequences_chr1.fa.gz')
-        unzipped_fasta_file = gunzip_file(fasta_path)
+        combined_df = self.combined_df.copy()
         output_dir  = tempfile.TemporaryDirectory() #output_dir.name
         output_file = os.path.join(output_dir.name, 'sample_transcripts.fa')
 
@@ -36,9 +40,9 @@ class JunctionToFastaTests(unittest.TestCase):
             junction = combined_df.loc[[i], :]
             for row in junction.itertuples():
                 junction_params = {
-                    'fasta_path'     : unzipped_fasta_file,
+                    'fasta_path'     : self.fasta_file,
                     'junction_df'    : combined_df,
-                    'gtf_df'         : gtf_df,
+                    'gtf_df'         : self.gtf_df,
                     'tscript_id'     : row.transcript_id,
                     'chrom'          : row.junction_chrom,
                     'junction_name'  : row.name,
@@ -50,8 +54,8 @@ class JunctionToFastaTests(unittest.TestCase):
                     'gene_name'      : row.gene_name,
                     'output_file'    : output_file,
                     'output_dir'     : output_dir.name,
-                    'sample_name'    : sample_name,
-                    'vcf'            : annotated_vcf,
+                    'sample_name'    : self.sample_name,
+                    'vcf'            : self.annotated_vcf,
                     'downstream_sequence_length': 1000,
                 }
                 junctions = JunctionToFasta(**junction_params)
@@ -83,17 +87,10 @@ class JunctionToFastaTests(unittest.TestCase):
                 "files don't match {} - {}".format(output_file, expected_file)
             )
 
-        os.unlink(unzipped_fasta_file)
-
         output_dir.cleanup()
 
     def test_downstream_sequence_length(self):
-        combined_df   = pd.read_csv(os.path.join(self.test_data_dir, 'results', 'Test.combined.tsv'), sep='\t', na_values="NA", dtype={'transcript_support_level': str})
-        gtf_df        = pd.read_csv(os.path.join(self.test_data_dir, 'results', 'Test.gtf.tsv'), sep='\t', na_values="NA", dtype={'transcript_support_level': str})
-        annotated_vcf = os.path.join(pvactools_directory(), self.test_data_dir, 'inputs', 'annotated.expression_chr1.vcf.gz')
-        sample_name   = 'HCC1395_TUMOR_DNA'
-        fasta_path = os.path.join(self.test_data_dir, 'inputs', 'all_sequences_chr1.fa.gz')
-        unzipped_fasta_file = gunzip_file(fasta_path)
+        combined_df = self.combined_df.copy()
         output_dir  = tempfile.TemporaryDirectory() #output_dir.name
         output_file = os.path.join(output_dir.name, 'sample_transcripts.fa')
 
@@ -101,9 +98,9 @@ class JunctionToFastaTests(unittest.TestCase):
             junction = combined_df.loc[[i], :]
             for row in junction.itertuples():
                 junction_params = {
-                    'fasta_path'     : unzipped_fasta_file,
+                    'fasta_path'     : self.fasta_file,
                     'junction_df'    : combined_df,
-                    'gtf_df'         : gtf_df,
+                    'gtf_df'         : self.gtf_df,
                     'tscript_id'     : row.transcript_id,
                     'chrom'          : row.junction_chrom,
                     'junction_name'  : row.name,
@@ -115,8 +112,8 @@ class JunctionToFastaTests(unittest.TestCase):
                     'gene_name'      : row.gene_name,
                     'output_file'    : output_file,
                     'output_dir'     : output_dir.name,
-                    'sample_name'    : sample_name,
-                    'vcf'            : annotated_vcf,
+                    'sample_name'    : self.sample_name,
+                    'vcf'            : self.annotated_vcf,
                     'downstream_sequence_length': 10,
                 }
                 junctions = JunctionToFasta(**junction_params)
@@ -147,7 +144,5 @@ class JunctionToFastaTests(unittest.TestCase):
                 expected_file),
                 "files don't match {} - {}".format(output_file, expected_file)
             )
-
-        os.unlink(unzipped_fasta_file)
 
         output_dir.cleanup()
