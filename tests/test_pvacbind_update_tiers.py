ef5012547404dcac975e21fb293c42f376d93176
Susanna Kiwala <susanna.kiwala@wustl.edu>
2026-07-20 12:56:47 -0500

    Use tmp files in pvac update_tiers tests

--- tests/test_pvacbind_update_tiers.py
+++ tests/test_pvacbind_update_tiers.py
@@ -5,6 +5,8 @@ import sys
 import py_compile
 from subprocess import PIPE
 from subprocess import run as subprocess_run
+import shutil
+import tempfile
 
 from pvactools.tools.pvacbind import *
 from tests.utils import *
@@ -53,4 +55,7 @@ class PvacbindUpdateTiersTests(unittest.TestCase):
 
     def test_runs(self):
         input_file = os.path.join(self.test_data_directory, 'MHC_Class_I', 'Test.MHC_I.all_epitopes.aggregated.tsv')
-        self.assertFalse(update_tiers.main([input_file]))
+        tmp_input_file = tempfile.NamedTemporaryFile()
+        shutil.copy(input_file, tmp_input_file.name)
+        self.assertFalse(update_tiers.main([tmp_input_file.name]))
+        os.unlink(tmp_input_file.name)
--- tests/test_pvacsplice_update_tiers.py
+++ tests/test_pvacsplice_update_tiers.py
@@ -5,6 +5,8 @@ import sys
 import py_compile
 from subprocess import PIPE
 from subprocess import run as subprocess_run
+import shutil
+import tempfile
 
 from pvactools.tools.pvacsplice import *
 from tests.utils import *
@@ -53,4 +55,7 @@ class PvacspliceUpdateTiersTests(unittest.TestCase):
 
     def test_runs(self):
         input_file = os.path.join(self.test_data_directory, 'results', 'run', 'MHC_Class_I', 'HCC1395_TUMOR_DNA.MHC_I.all_epitopes.aggregated.tsv')
-        self.assertFalse(update_tiers.main([input_file, "0.5"]))
+        tmp_input_file = tempfile.NamedTemporaryFile()
+        shutil.copy(input_file, tmp_input_file.name)
+        self.assertFalse(update_tiers.main([tmp_input_file.name, "0.5"]))
+        os.unlink(tmp_input_file.name)
