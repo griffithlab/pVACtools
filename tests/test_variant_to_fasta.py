781c0fd8757388da6a136a762506e76458983c41
Susanna Kiwala <susanna.kiwala@wustl.edu>
2026-07-22 15:44:30 -0500

    Restore repetitive deletion tests

--- tests/test_variant_to_fasta.py
+++ tests/test_variant_to_fasta.py
@@ -557,5 +557,21 @@ class VariantToFastaTests(unittest.TestCase):
         expected_output_file = os.path.join(self.test_data_dir, 'output_frameshift_variant_position_1.fasta')
         self.assertTrue(cmp(generate_fasta_output_file.name, expected_output_file))
 
+    def test_input_file_with_repetitive_deletion_generates_expected_file(self):
+        generate_fasta_input_file      = os.path.join(self.test_data_dir, 'input_repetitive_deletion.tsv')
+        generate_fasta_output_file     = tempfile.NamedTemporaryFile()
+
+        generate_fasta_params = {
+            'input_file'                : generate_fasta_input_file,
+            'output_file'               : generate_fasta_output_file.name,
+            'downstream_sequence_length': None,
+            'proximal_variants_file'    : None,
+        }
+        generator = VariantToFasta(**generate_fasta_params)
+
+        self.assertFalse(generator.execute())
+        expected_output_file = os.path.join(self.test_data_dir, 'output_repetitive_deletion.fasta')
+        self.assertTrue(cmp(generate_fasta_output_file.name, expected_output_file))
+
 if __name__ == '__main__':
     unittest.main()
