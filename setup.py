08b34eb8de35125520671490ca4cdc17797b360a
Susanna Kiwala <susanna.kiwala@wustl.edu>
2026-08-12 08:59:46 -0500

    Update to version 6.1.2

--- setup.py
+++ setup.py
@@ -65,7 +65,7 @@ for dirpath, dirnames, filenames in os.walk("pvactools/tools/pvaccompare/html_re
 
 setup(
     name="pvactools",
-    version="6.1.1",
+    version="6.1.2",
     packages=[
         "pvactools.tools",
         "pvactools.tools.pvacbind",
