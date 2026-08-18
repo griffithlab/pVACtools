dc4725f0cd02836d766e48eae457959693539a74
Susanna Kiwala <susanna.kiwala@wustl.edu>
2026-08-18 08:35:03 -0500

    Update create peptide ordering form commands to take in a transcripts.fa file

--- pvactools/tools/pvacseq/create_peptide_ordering_form.py
+++ pvactools/tools/pvacseq/create_peptide_ordering_form.py
@@ -13,19 +13,13 @@ def main(args_input = sys.argv[1:]):
     args = parser.parse_args(args_input)
 
     params = {
-        'input_vcf': args.input_vcf,
+        'transcripts_fasta': args.transcripts_fasta,
         'flanking_sequence_length': args.flanking_sequence_length,
         'classI_aggregated_tsv': args.classI_aggregated_tsv,
         'classII_aggregated_tsv': args.classII_aggregated_tsv,
         'output_file_prefix': args.output_file_prefix,
         'sample_name': args.sample_name,
         'output_path': args.output_path,
-        'phased_proximal_variants_vcf': args.phased_proximal_variants_vcf,
-        'external_vcf': args.external_vcf,
-        'pass_only': args.pass_only,
-        'biotypes': args.biotypes,
-        'allow_incomplete_transcripts': args.allow_incomplete_transcripts,
-        'downstream_sequence_length': args.downstream_sequence_length,
         'aggregate_report_evaluation': args.aggregate_report_evaluation,
         'classI_IC50': args.classI_IC50,
         'classI_percent': args.classI_percent,
